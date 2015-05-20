5cfa01342b1e2ab0ff253c7fe8d3e6d500d60235
mhedbom <mathias.hedbom@ha.ax>
2015-05-20 10:49:41 +0300

    substring is now correct length in readOutput

--- xBee/xBee.cpp
+++ xBee/xBee.cpp
@@ -34,7 +34,7 @@ std::string xBee::readOutput(int fd){
     	std::cout << "XML message start and end tags found!" << '\n' << 
     			     "Start tag pos: " << pos_start << '\n' << "End tag pos: " << pos_end << std::endl;
 
-		std::string result = printer.substr(pos_start, pos_end); 
+		std::string result = printer.substr(pos_start, pos_end + pos_start); 
 		std::cout << "Resulting XML: " << result << std::endl;
 	}
 
