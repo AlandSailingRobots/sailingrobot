a6b41c15c9ed8c0bbb8b0a78ea2fac680e5be98c
Emil Pettersson <emil.pettersson@ha.ax>
2016-04-05 12:36:03 +0000

    Added files xBeeSync.*

---
+++ xBee/xBeeSync.cpp
@@ -0,0 +1,124 @@
+#include "xBeeSync.h"
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+xBeeSync::xBeeSync(ExternalCommand* externalCommand, SystemState *systemState,
+				   DBHandler* db, bool sendLogs, bool sending, bool receiving) :
+	m_external_command(externalCommand),
+	m_model(
+		SystemStateModel(
+			GPSModel("",PositionModel(0,0),0,0,0,0),
+			WindsensorModel(0,0,0),
+			CompassModel(0,0,0),
+			0,
+			0
+		)
+	),
+	m_system_state(systemState),
+	m_db(db),
+	m_running(true),
+	m_sending(sending),
+	m_receiving(receiving),
+	m_sendLogs(sendLogs)
+{
+	//	skapa ny xBee och kör xbee.init(baudRate (57600))
+	m_xbee_fd = m_xBee.init(57600);
+	std::cout << "*xBee initialized - receiving:" << m_receiving
+			  << " sending:" << m_sending << std::endl;
+	m_logger.info(std::string(
+		"*xBee initialized - receiving:") +	std::to_string(m_receiving)	+
+		" sending:" + std::to_string(m_sending));
+}
+
+void xBeeSync::run()
+{
+	std::cout << "*xBeeSync thread started." << std::endl;
+	m_logger.info("*xBeeSync thread started.");
+
+	while(isRunning()) 
+	{
+		m_system_state->getData(m_model);
+
+		if(m_sending) {
+			if(m_sendLogs) {
+				std::string logs = m_db->getLogs();
+				m_xBee.transmitData(m_xbee_fd, logs);
+
+			} else {
+				std::string res_xml = m_XML_log.log_xml(
+					m_model.gpsModel.timestamp,					// Timestamp
+					m_model.windsensorModel.direction,			// winddir degrees
+					m_model.windsensorModel.speed, 				// windspeed ms
+					m_model.compassModel.heading, 				// Heading deg
+					m_model.compassModel.pitch, 				// Pitch deg
+					m_model.compassModel.roll, 					// Roll deg
+					m_model.gpsModel.positionModel.latitude, 	// gml:pos arg1, lat
+					m_model.gpsModel.positionModel.longitude, 	// gml:pos arg2, long
+					m_model.gpsModel.heading, 					// course over ground(deg)
+					m_model.gpsModel.speed, 					// speed over ground(ms)
+					m_model.rudder,								// Rudderpos
+					m_model.sail								// Sailpos
+				);
+
+				// Kan skicka loggen direkt med:
+				m_xBee.transmitData(m_xbee_fd,res_xml);
+			}
+		}
+		
+		if(m_receiving)
+		{
+			std::string res_xml = m_xBee.receiveXMLData(m_xbee_fd);
+ 			
+			//If return value equals -1, parsing failed...
+ 			int rudder_cmd = m_XML_log.parse_rudCMD(res_xml);
+	 		int sail_cmd = m_XML_log.parse_saiCMD(res_xml);
+			std::string timestamp = m_XML_log.parse_time(res_xml);
+
+	 		if(timestamp.length() > 0) {
+	 			std::cout << "Timestamp in xBeeSync::run = " << timestamp << std::endl;
+				m_logger.info("Timestamp in xBeeSync::run = " + timestamp);
+	 		}
+	 		if(rudder_cmd != -1) {
+	 			std::cout << "Rudder command in xBeeSync::run = " << rudder_cmd << std::endl;	
+	 			m_logger.info(std::string("Rudder command in xBeeSync::run = ")
+	 				+ std::to_string(rudder_cmd));
+	 			m_model.rudder = rudder_cmd;
+	 		}
+	 		if(sail_cmd != -1) {
+	 			std::cout << "Sail command in xBeeSync::run = " << sail_cmd << std::endl;	
+	 			m_logger.info(std::string("Sail command in xBeeSync::run = ")
+	 				+ std::to_string(sail_cmd));
+	 			m_model.sail = sail_cmd;
+	 		}
+
+	 		bool autorun = false;
+	 		
+	 		if(sail_cmd != -1 && rudder_cmd != -1){
+	 			m_external_command->setData(timestamp, autorun, rudder_cmd, sail_cmd);
+	 		}
+		}
+
+		//make sure there are at least 0.4 seconds between each xml message
+		std::this_thread::sleep_for(
+			std::chrono::milliseconds(400));
+	}
+	std::cout << "*xBeeSync thread exited." << std::endl;
+	m_logger.info("*xBeeSync thread exited.");
+}
+
+void xBeeSync::close()
+{
+	m_mutex.lock();
+	m_running = false;
+	m_mutex.unlock();
+}
+
+bool xBeeSync::isRunning()
+{
+	bool running;
+	m_mutex.lock();
+	running = m_running;
+	m_mutex.unlock();
+	return running;
+}
---
+++ xBee/xBeeSync.h
@@ -0,0 +1,42 @@
+#ifndef __XBEESYNC_H__
+#define __XBEESYNC_H__
+
+#include "xBee/xBee.h"
+#include "xmlparser/src/xml_log.h"
+#include "thread/ExternalCommand.h"
+#include "thread/SystemState.h"
+#include "models/SystemStateModel.h"
+#include "logger/Logger.h"
+#include "dbhandler/DBHandler.h"
+#include <mutex>
+
+class xBeeSync
+{
+public:
+	xBeeSync(ExternalCommand* externalCommand, SystemState *systemState,
+			 DBHandler* db, bool sendLogs, bool sending, bool receiving);
+	~xBeeSync() {};
+
+	void run();
+	void close();
+	
+private:
+	XML_log m_XML_log;
+	xBee m_xBee;
+	int m_xbee_fd;
+	ExternalCommand* m_external_command;
+	SystemStateModel m_model;
+	SystemState *m_system_state;
+	DBHandler *m_db;
+
+	std::mutex m_mutex;
+	bool m_running;
+	bool m_sending;
+	bool m_receiving;
+	bool m_sendLogs;
+	Logger m_logger;
+
+	bool isRunning();
+};
+
+#endif
