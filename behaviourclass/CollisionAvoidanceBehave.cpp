eba15ed51b91e068d451b167fd4d9c2903971994
mbrandtb <mathias.brandtberg@ha.ax>
2016-07-05 13:23:22 +0300

    added functions skeletton

--- behaviourclass/CollisionAvoidanceBehave.cpp
+++ behaviourclass/CollisionAvoidanceBehave.cpp
@@ -11,32 +11,28 @@ bool CollisionAvoidanceBehave::init()
 }
 
 void CollisionAvoidanceBehave::computeCommands(SystemStateModel &systemStateModel,std::unique_ptr<Position> const& position,
-                                  double trueWindDirection, bool mockPosition,
-                                  bool getHeadingFromCompass){
-}
+                                    double trueWindDirection, bool mockPosition,
+                                    bool getHeadingFromCompass) {}
 
-
-void CollisionAvoidanceBehave::manageDatabase(double trueWindDirection,SystemStateModel &m_systemStateModel){
-
-}
+void CollisionAvoidanceBehave::manageDatabase(double trueWindDirection, SystemStateModel &systemStateModel){}
 void CollisionAvoidanceBehave::setupWaypoints(){}
 void CollisionAvoidanceBehave::setNextWaypoint(){}
 void CollisionAvoidanceBehave::setPreviousWayPoint(SystemStateModel &m_systemStateModel){}
-
 int CollisionAvoidanceBehave::getHeading(SystemStateModel &m_systemStateModel,bool m_mockPosition,bool m_getHeadingFromCompass,std::unique_ptr<Position> const& position) {
     return 1;
 }
-/*
-MatrixXd CollisionAvoidanceBehave::createWall(MatrixXd const& starting_point,MatrixXd const& ending_point,float step){
-    Eigen::MatrixXd m(2,1);
+
+void CollisionAvoidanceBehave::mockObstacleDetection(){}//Matlab name: calculate_potField
+void CollisionAvoidanceBehave::calculatePotentialField(){}//Matlab name: calculate_potField
+void CollisionAvoidanceBehave::avoidObstacle(){}//Matlab name: avoid_obstacle
+void CollisionAvoidanceBehave::obstacleOnACollisionCourse(){}//Matlab name: boat_on_collision_course
+void CollisionAvoidanceBehave::calculate_collision_avoidance_point(){}//Matlab name: calculate_avoidCollisionPoint
+Eigen::MatrixXd CollisionAvoidanceBehave::createWall(Eigen::MatrixXd const& starting_point,Eigen::MatrixXd const& ending_point,float step){
+    Eigen::MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
     return m;
 }
-void CollisionAvoidanceBehave::obstacleOnACollisionCourse(MatrixXd const& boat_state, MatrixXd const& detected_obstacle_list_qhat){}
-void CollisionAvoidanceBehave::mockObstacleDetection(MatrixXd const& boat_state, MatrixXd const& mock_obstacle_list,std::vector<Eigen::MatrixXd> detected_obstacle_list_qhat){}
-
-void CollisionAvoidanceBehave::calculatePotentialField(MatrixXd const& point_x,MatrixXd const& point_y,
-                            MatrixXd const& boat_state,MatrixXd const& target_phat,
-                            MatrixXd const& detected_obstacle_list_qhat,
-                            MatrixXd& potential_Z){}
-void CollisionAvoidanceBehave::calculateAvoidancePoint(MatrixXd const& boat_state, MatrixXd const& target_phat, MatrixXd const& detected_obstacle_list_qhat,MatrixXd const& potential_Z, MatrixXd& avoidance_point){}
-*/
+void CollisionAvoidanceBehave::moveObstacle(std::vector<Eigen::MatrixXd>& mock_obstacle_list,std::vector<int> elements, float dt){}
--- behaviourclass/CollisionAvoidanceBehave.h
+++ behaviourclass/CollisionAvoidanceBehave.h
@@ -10,7 +10,6 @@
 #include "waypointrouting/WaypointRouting.h"
 #include "behaviourclass/LineFollowBehaviour.h"
 #include "libs/Eigen/Dense"
-using Eigen::MatrixXd;
 
 class CollisionAvoidanceBehave:public RoutingBehaviour{
 public:
@@ -18,11 +17,9 @@ public:
     ~CollisionAvoidanceBehave(){};
 
     bool init();
-
     void computeCommands(SystemStateModel &systemStateModel,std::unique_ptr<Position> const& position,
-                                      double trueWindDirection, bool mockPosition,
-                                      bool getHeadingFromCompass);  //make the computation of the commands return true if the computation was successfull
-
+                                        double trueWindDirection, bool mockPosition,
+                                        bool getHeadingFromCompass); 
     void manageDatabase(double trueWindDirection, SystemStateModel &systemStateModel);
 /*
     double getRudderCommand();
@@ -35,83 +32,127 @@ private:
     void setPreviousWayPoint(SystemStateModel &m_systemStateModel);
     int getHeading(SystemStateModel &m_systemStateModel,bool m_mockPosition,bool m_getHeadingFromCompass,std::unique_ptr<Position> const& position);
     //Potential Field
-    MatrixXd potential_Z;
-    MatrixXd point_x;
-    MatrixXd point_y;
-    float step_coeff;//In direction mode length of the repulsion of the obstacle
-    MatrixXd potential_field_dim; //xmin xmax ymin ymax
+    Eigen::MatrixXd potential_Z;//Matlab name: Z
+    Eigen::MatrixXd point_x;//Matlab name: P1
+    Eigen::MatrixXd point_y;//Matlab name: P2
+    float step_coeff;//Matlab name: eta //In direction mode length of the repulsion of the obstacle
+    Eigen::MatrixXd potential_field_dim; //Matlab name: dim //xmin xmax ymin ymax
 
     //Obstacles
-    std::vector<MatrixXd> mock_obstacle_list;//position qnd speed of mock obstacles
-    std::vector<MatrixXd> detected_obstacle_list_qhat;
-    std::vector<MatrixXd> detected_obstacles;
-    std::vector<MatrixXd> collisioned_obstacle;//Incle a memory of the previous encountered obstacles if not in direction mode
-    float direction_boat_obstacle;
-    float radius_obstacle;//security radius around the obstacle
-    MatrixXd collision_avoidance_point;// Point to follow when an obstacle is detected
-    int is_obstacle_detected; //1 if an obstacle is detected
+    std::vector<Eigen::MatrixXd> mock_obstacle_list;//Matlab name: posObstacles //position qnd speed of mock obstacles
+    std::vector<Eigen::MatrixXd> detected_obstacle_list_qhat;//Matlab name:  qhat
+    std::vector<Eigen::MatrixXd> detected_obstacles;//Matlab name: detectedObstacles
+    std::vector<Eigen::MatrixXd> collisioned_obstacle;//Matlab name: collisionedObstacle//Incle a memory of the previous encountered obstacles if not in direction mode
+    float direction_boat_obstacle;//Matlab name: directionObstacleDetected
+    float radius_obstacle;//Matlab name: rq  //security radius around the obstacle
+    Eigen::MatrixXd collision_avoidance_point;//Matlab name: avoidCollisionPoint // Point to follow when an obstacle is detected
+    int is_obstacle_detected;//Matlab name: is_obstacle_detected //1 if an obstacle is detected
 
     //Targets
-    std::vector<MatrixXd> target_list;
-    MatrixXd target_phat;
+    std::vector<Eigen::MatrixXd> target_list;//Matlab name: posWaypoints
+    Eigen::MatrixXd target_phat;//Matlab name: phat
 
     //Boat
-    MatrixXd boat_state;
-    float mock_detection_distance;
-    std::vector<MatrixXd> line_to_follow;
-    float radius_corridor;//corridor to stay in during line following
+    Eigen::MatrixXd boat_state;//Matlab name: x
+    float mock_detection_distance;//Matlab name: distDetect//Range of detection of obstacles
+    float mock_detection_angle;//Matlab name: angleDetect//Angle of detection of obstacles
+    std::vector<Eigen::MatrixXd> line_to_follow;//Matlab name: followedLine
+    float radius_corridor;//Matlab name: r//corridor to stay in during line following
+
 
     //World
-    float wind_direction;
+    float wind_direction;//Matlab name: psi
 
     //Different mode
-    int only_direction_mode; //Do the boat know the position or the direction of the obstacle?
-    int have_to_avoid_obstacle;//In direction mode do the boat need to avoid an obstacle?
-    int can_compute_a_new_avoidance_point;//Can the boat compute a new collision_avoidance_point
+    int only_direction_mode;//Matlab name:  onlyHeadingMode //Do the boat know the position or the direction of the obstacle?
+    int have_to_avoid_obstacle;//Matlab name: haveToAvoidObstacle //In direction mode do the boat need to avoid an obstacle?
+    int can_compute_a_new_avoidance_point;//Matlab name: avoidMode //Can the boat compute a new collision_avoidance_point
+
     /*
-    void mockObstacleDetection(MatrixXd const& boat_state, std::vector<Eigen::MatrixXd> const& mock_obstacle_list,float mock_detection_distance,float mock_detection_distance,
-                                std::vector<Eigen::MatrixXd>& detected_obstacles, float& direction_boat_obstacle);
+    mockObstacleDetection() : fake the detection of an obstacle according to an mock_detection_angle and a mock_detection_distance
+    Uses:
+        MatrixXd boat_state,
+        std::vector<Eigen::MatrixXd> mock_obstacle_list
+        float mock_detection_distance,
+        float mock_detection_angle,
+    Modified attributes:
+        std::vector<Eigen::MatrixXd> detected_obstacles,
+        float direction_boat_obstacle
+    */
+    void mockObstacleDetection();//Matlab name: calculate_potField
 
-    void calculatePotentialField(MatrixXd const& point_x,
-                                 MatrixXd const& point_y,
-                                 MatrixXd const& boat_state,
-                                 MatrixXd const& target_phat,
-                                 std::vector<Eigen::MatrixXd> const& detected_obstacle_list_qhat,
-                                 float wind_direction,
-                                 int only_direction_mode,
-                                 int have_to_avoid_obstacle,
-                                 float step_coeff,
-                                 float direction_boat_obstacle,
-                                 MatrixXd& potential_Z);
-
-    void avoidObstacle(int& can_compute_a_new_avoidance_point,
-                       std::vector<Eigen::MatrixXd>& line_to_follow,
-                       MatrixXd const& collisioned_Obstacle,
-                       MatrixXd& avoidance_collision_point,
-                       std::vector<Eigen::MatrixXd>& detected_obstacle_list_qhat,
-                       MatrixXd const& target_phat,
-                       MatrixXd const& boat_state,
-                       MatrixXd const& point_x,
-                       MatrixXd const& point_y,
-                       float wind_direction,
-                       float radius_obstacle,
-                       float radius_corridor,
-                       float dim,
-                       int only_direction_mode,
-                       int have_to_avoid_obstacle,
-                       float step_coeff,
-                       float direction_boat_obstacle);
-
-    void obstacleOnACollisionCourse(MatrixXd const& boat_state,
-                                    std::vector<Eigen::MatrixXd> const& detected_obstacle_list_qhat,
-                                    float radius_obstacle,
-                                    int can_compute_a_new_avoidance_point);
-
-
+/*
+    calculatePotentialField : Potential field computation to place a collision_avoidance_point in its global minimum
+    Uses:
+        MatrixXd point_x,
+        MatrixXd point_y,
+        MatrixXd boat_state,
+        MatrixXd target_phat,
+        std::vector<Eigen::MatrixXd> detected_obstacle_list_qhat,
+        float wind_direction,
+        int only_direction_mode,
+        int have_to_avoid_obstacle,
+        float step_coeff,
+        float direction_boat_obstacle,
+    Modified attributes:
+        MatrixXd potential_Z
+*/
+    void calculatePotentialField();//Matlab name: calculate_potField
+/*
+    avoidObstacle : Do the computation to avoid the obstacle
+    Uses:
+        MatrixXd target_phat,
+        MatrixXd boat_state,
+        MatrixXd point_x,
+        MatrixXd point_y,
+        float wind_direction,
+        float radius_obstacle,
+        float radius_corridor,
+        float dim,
+        int only_direction_mode,
+        int have_to_avoid_obstacle,
+        float step_coeff,
+        float direction_boat_obstacle
+    Modified attributes:
+        int can_compute_a_new_avoidance_point,
+        std::vector<Eigen::MatrixXd> line_to_follow,
+        MatrixXd const collisioned_Obstacle,
+        MatrixXd avoidance_collision_point,
+        std::vector<Eigen::MatrixXd> detected_obstacle_list_qhat,
 
 */
-MatrixXd createWall(MatrixXd const& starting_point,MatrixXd const& ending_point,float step);
-moveObstacle(std::vector<MatrixXd>& mock_obstacle_list,std::vector<int> elements, float dt);
+    void avoidObstacle();//Matlab name: avoid_obstacle
+/*
+    obstacleOnACollisionCourse: check if an object is on the boat course. Take into account a memory of the already encountered obstacles:
+    Uses:
+        MatrixXdboat_state,
+        std::vector<Eigen::MatrixXd> detected_obstacle_list_qhat,
+        float radius_obstacle,
+        int can_compute_a_new_avoidance_point,
+        std::vector<MatrixXd> detected_obstacles,
+    Modified attributes:
+        MatrixXd collisioned_Obstacle,
+        int only_direction_mode,
+        int is_obstacle_detected;
+        int have_to_avoid_obstacle;
+
+*/
+    void obstacleOnACollisionCourse();//Matlab name: boat_on_collision_course
+    /*
+    calculate_collision_avoidance_point: compute the global minimu of the Z field and return it
+    Uses:
+        MatrixXd potential_Z;
+        MatrixXd potential_field_dim;
+    Modified attributes:
+        std::vector<MatrixXd> collisioned_obstacle;
+    */
+
+    void calculate_collision_avoidance_point();//Matlab name: calculate_avoidCollisionPoint
+
+
+    Eigen::MatrixXd createWall(Eigen::MatrixXd const& starting_point,Eigen::MatrixXd const& ending_point,float step);
+
+    void moveObstacle(std::vector<Eigen::MatrixXd>& mock_obstacle_list,std::vector<int> elements, float dt);
 
 };
 
