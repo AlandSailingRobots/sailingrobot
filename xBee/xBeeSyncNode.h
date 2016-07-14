3cf209f24ceef7c1705646da4421f9e02bcb6302
EMIL S <william.soderlund@ha.ax>
2016-07-14 10:48:43 +0300

    Initial structure and renaming
    Still far from functioning - see comments

---
+++ xBee/xBeeSyncNode.h
@@ -0,0 +1,49 @@
+#ifndef __XBEESYNC_H__
+#define __XBEESYNC_H__
+
+#include "xBee/xBee.h"
+#include "xmlparser/src/xml_log.h"
+#include "thread/ExternalCommand.h"
+#include "thread/SystemState.h"
+#include "models/SystemStateModel.h"
+#include "logger/Logger.h"
+#include "dbhandler/DBHandler.h"
+#include "utility/Timer.h"
+#include <mutex>
+
+class xBeeSyncNode
+{
+public:
+	xBeeSyncNode(DBHandler* db, bool sendLogs, bool sending, bool receiving, double loopTime);
+	~xBeeSyncNode() {};
+
+	/////////////////////////////////////////////////////////////////////////////////////
+ 	/// Initialises the xBee module and returns false if the function fails to setup the
+ 	/// xbee. 
+ 	///
+ 	/////////////////////////////////////////////////////////////////////////////////////
+	bool init();
+
+	void xBeeSyncThread();
+	void close();
+	
+private:
+	XML_log m_XML_log;
+	xBee m_xBee;
+	int m_xbee_fd;
+	SystemStateModel m_model;
+	DBHandler *m_db;
+	Timer m_timer;
+
+	std::mutex m_mutex;
+	bool m_running;
+	bool m_sending;
+	bool m_receiving;
+	bool m_sendLogs;
+	bool m_pushOnlyLatestLogs;
+	double m_loopTime;
+
+	bool isRunning();
+};
+
+#endif
