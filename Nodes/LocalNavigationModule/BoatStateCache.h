80c34ec4611a0c7a0bd54ac6ce5c49e649f4ec62
Jordan Less'ard-Springet <jordan80100@gmail.com>
2017-02-26 13:08:34 +0000

    Added the BoatStateCache

---
+++ Nodes/LocalNavigationModule/BoatStateCache.h
@@ -0,0 +1,33 @@
+/****************************************************************************************
+ *
+ * File:
+ * 		BoatStateCache.h
+ *
+ * Purpose:
+ *		A cache the Local Navigation Module uses to store boat data.
+ *
+ * License:
+ *      This file is subject to the terms and conditions defined in the file 
+ *      'LICENSE.txt', which is part of this source code package.
+ *
+ ***************************************************************************************/
+
+
+#pragma once
+
+
+#include <stdint.h>
+
+
+struct BoatStateCache_t {
+    double currWaypointLat;
+    double currWaypointLon;
+    double lastWaypointLat;
+    double lastWaypointLon;
+
+    uin16_t heading;
+    double lat;
+    double lon;
+    uint16_t wind;
+    double speed;
+};
