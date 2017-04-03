e9e3f173f4d46094eb58b2436711acf79b1c2e33
Elessog <elouan.autret@ensta-bretagne.org>
2017-04-03 13:53:29 +0300

    Removing the constants from CANWindsensorNode

--- SystemServices/CANWindsensorNode.cpp
+++ SystemServices/CANWindsensorNode.cpp
@@ -52,7 +52,7 @@ void CANWindsensorNode::start()
 void CANWindsensorNode::processPGN(N2kMsg &NMsg, uint32_t PGN)
 {
 	//TODO:: parse message 
-	if(PGN == PGN130306){
+	if(PGN == 130306){
 		uint8_t SID, Ref;
 		float WS, WA;
 		parsePGN130306(N2kMsg &Msg, SID, WS, WA, Ref);
@@ -61,7 +61,9 @@ void CANWindsensorNode::processPGN(N2kMsg &NMsg, uint32_t PGN)
 		std::cout << "Windangle: "<< WA << " ";
 		std::cout << "Reference: "<< (int)Ref << std::endl;
 	}
-	else if()
+	else if(PGN == 130311){
+		
+	}
 }
 
 void CANWindsensorNode::parsePGN130306(N2kMsg &NMsg, uuint8_t &SID, float &WindSpeed,				//WindData
@@ -73,4 +75,20 @@ void CANWindsensorNode::parsePGN130306(N2kMsg &NMsg, uuint8_t &SID, float &WindS
 	tmp = NMsg.Data[3] | (NMsg.Data[4]<<8);
 	WindAngle = tmp*0.0001;
 	Reference = NMsg.Data[5] & 0x07;
+}
+
+void CANWindsensorNode::parsePGN130311(N2kMsg &Msg, uint8_t &SID, uint8_t &TemperatureInstance,	//Environmental Parameters
+	uint8_t &HumidityInstance, float &Temperature,
+	float &Humidity, float &AtmosphericPressure)
+{
+	SID = NMsg.Data[0];
+	TemperatureInstance = NMsg.Data[1] & 0x3f;
+	HumidityInstance = NMsg.Data[1] >> 6;
+	uint16_t tmp = NMsg.Data[2] | (NMsg.Data[3]<<8);
+	Temperature = tmp*0.01;
+	//tmp = NMsg.Data[4] | (NMsg.Data[5]<<8);
+	//Humidity = tmp*0.004;
+	Humidity = 0;
+	tmp = NMsg.Data[6] | (NMsg.Data[7]<<8);
+	AtmosphericPressure = tmp;		//hPa
 }
