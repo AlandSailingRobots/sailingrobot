24310298a6cefb56f84a78dd8b461a3f02b96717
Elessog <elouan.autret@ensta-bretagne.org>
2017-04-03 12:56:02 +0300

    adding the PGNs for the windsensor

--- SystemServices/CANWindsensorNode.h
+++ SystemServices/CANWindsensorNode.h
@@ -33,14 +33,28 @@ public:
 
 
 	///----------------------------------------------------------------------------------
- 	/// Processes CANWindsensorMsgs
+ 	/// Attempts to connect to the CV7 wind sensor.
  	///
  	///----------------------------------------------------------------------------------
-	virtual void processMessage(const Message* message);
+	bool init();
+
+	///----------------------------------------------------------------------------------
+ 	/// Starts the wind sensors thread so that it actively pumps data into the message
+ 	/// bus.
+ 	///
+ 	///----------------------------------------------------------------------------------
+	void start();
 
 private:
+	bool m_Initialised;		// Indicates that the node was correctly initialised
 	float m_WindDir;
 	float m_WindSpeed;
 	float m_WindTemperature;
+
+	//Windsensor
+	const PGN130306 = 130306; 
+	const PGN130311 = 130311;
+	const PGN130312 = 130312;
+	const PGN130314 = 130314;
 
 };
