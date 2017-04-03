a542b9fc5ed70bb3209f792b586fb48641b59342
Elessog <elouan.autret@ensta-bretagne.org>
2017-04-03 09:29:54 +0300

    Removing the old canpgnrecieve.h and CANPGNReceiver.h

--- SystemServices/CANPGNReceiver.h
+++ SystemServices/CANPGNReceiver.h
@@ -1,13 +1,13 @@
 
-#ifndef canpgnrecieve_h
-#define canpgnrecieve_h
+#ifndef canpgnreciever_h
+#define canpgnreciever_h
 
 #include <stdint.h>
 #include <vector>
 
-class CanPGNRecieve
+class CanPGNReceiver
 {
-	
+
 public:
 	virtual void processPGN(std::vector<uint8_t> Data, uint32_t PGN) = 0;
 };
