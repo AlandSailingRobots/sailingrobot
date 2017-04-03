d869be3328646538d4ff55385c0c11583d6aad7b
Elessog <elouan.autret@ensta-bretagne.org>
2017-04-03 13:58:10 +0300

    Implementaion for parsing PGN130312 message

--- SystemServices/CANWindsensorNode.cpp
+++ SystemServices/CANWindsensorNode.cpp
@@ -52,7 +52,7 @@ void CANWindsensorNode::start()
 void CANWindsensorNode::processPGN(N2kMsg &NMsg, uint32_t PGN)
 {
 	//TODO:: parse message 
-	if(PGN == 130306){
+	if(NMsg.PGN == 130306){
 		uint8_t SID, Ref;
 		float WS, WA;
 		parsePGN130306(N2kMsg &Msg, SID, WS, WA, Ref);
@@ -61,8 +61,26 @@ void CANWindsensorNode::processPGN(N2kMsg &NMsg, uint32_t PGN)
 		std::cout << "Windangle: "<< WA << " ";
 		std::cout << "Reference: "<< (int)Ref << std::endl;
 	}
-	else if(PGN == 130311){
-		
+	else if(NMsg.PGN == 130311){
+		uint8_t SID, TI, HI;
+		float Temp, Hum, AP;
+		ParsePGN130311(NMsg, SID, TI, HI, Temp, Hum, AP);
+		std::cout << "SID: " << (int)SID << " ";
+		std::cout << "TemperatureInstance: " << (int)TI << " ";
+		std::cout << "HumidityInstance: " << (int)HI << " ";
+		std::cout << "Temperature: " << Temp << " ";
+		std::cout << "Humidity: " << Hum << " ";
+		std::cout << "AtmosphericPressure: " << AP << std::endl;
+	}
+	else if(NMsg.PGN == 130312){
+		uint8_t SID, TI, TS;
+		float ATemp, STemp;
+		ParsePGN130312(NMsg, SID, TI, TS, ATemp, STemp);
+		std::cout << "SID: " << (int)SID << " ";
+		std::cout << "TemperatureInstance: " << (int)TI << " ";
+		std::cout << "TemperatureSource: " << (int)TS << " ";
+		std::cout << "Actual Temperature: " << ATemp << " ";
+		std::cout << "Set Temperature: " << STemp << std::endl;
 	}
 }
 
@@ -91,4 +109,17 @@ void CANWindsensorNode::parsePGN130311(N2kMsg &Msg, uint8_t &SID, uint8_t &Tempe
 	Humidity = 0;
 	tmp = NMsg.Data[6] | (NMsg.Data[7]<<8);
 	AtmosphericPressure = tmp;		//hPa
+}
+
+void CANWindsensorNode::ParsePGN130312(N2kMsg &NMsg, uint8_t &SID, uint8_t &TemperatureInstance,	//Temperature
+					uint8_t &TemperatureSource, float &ActualTemperature,
+					float &SetTemperature)
+{
+	SID = NMsg.Data[0];
+	TemperatureInstance = NMsg.Data[1];
+	TemperatureSource = NMsg.Data[2];
+	uint16_t tmp = NMsg.Data[3] | (NMsg.Data[4]<<8);
+	ActualTemperature = tmp*0.01;
+	tmp = NMsg.Data[5] | (NMsg.Data[6]<<8);
+	SetTemperature = tmp*0.01;
 }
