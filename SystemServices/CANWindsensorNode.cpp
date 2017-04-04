7bcfbede8310bec410972ea939b5fbe02b0d874a
Elessog <elouan.autret@ensta-bretagne.org>
2017-04-04 08:28:19 +0300

    refactoring

--- SystemServices/CANWindsensorNode.cpp
+++ SystemServices/CANWindsensorNode.cpp
@@ -16,14 +16,14 @@
 
 
  CANWindsensorNode::CANWindsensorNode(MessageBus& msgBus, float windDir, float windSpeed, float windTemperature)
- 	:Node(NodeID::WindSensor, msgBus), m_WindDir(windDir), m_WindSpeed(m_WindSpeed), m_WindTemperature(windTemperature)
+ 	:Node(NodeID::WindSensor, msgBus), m_WindDir(windDir), m_WindSpeed(windSpeed), m_WindTemperature(windTemperature)
  {
 
  }
 
  bool CANWindsensorNode::init()
 {
- return true;
+ 	return true;
 }
 
 void CANWindsensorNode::processPGN(N2kMsg &NMsg, uint32_t PGN)
@@ -36,7 +36,7 @@ void CANWindsensorNode::processPGN(N2kMsg &NMsg, uint32_t PGN)
 		m_WindDir = WA;
 		m_WindSpeed = WS;
 
-		MessagePtr windData = std::make_unique<WindDataMsg>(NMsg.Source, this->nodeID(), m_WindDir, m_WindTemperature, m_WindSpeed);
+		MessagePtr windData = std::make_unique<WindDataMsg>(m_WindDir, m_WindTemperature, m_WindSpeed);
 		m_MsgBus.sendMessage(std::move(windData));
 
 	}
@@ -44,25 +44,25 @@ void CANWindsensorNode::processPGN(N2kMsg &NMsg, uint32_t PGN)
 	{
 		uint8_t SID, TI, HI;
 		float Temp, Hum, AP;
-		ParsePGN130311(NMsg, SID, TI, HI, Temp, Hum, AP);
+		parsePGN130311(NMsg, SID, TI, HI, Temp, Hum, AP);
 
 		m_WindTemperature = Temp;
 
-		MessagePtr windData = std::make_unique<WindDataMsg>(NMsg.Source, this->nodeID(), m_WindDir, m_WindTemperature, m_WindSpeed);
+		MessagePtr windData = std::make_unique<WindDataMsg>(m_WindDir, m_WindTemperature, m_WindSpeed);
 		m_MsgBus.sendMessage(std::move(windData));
 	}
 	else if(NMsg.PGN == 130312)
 	{
 		uint8_t SID, TI, TS;
 		float ATemp, STemp;
-		ParsePGN130312(NMsg, SID, TI, TS, ATemp, STemp);
+		parsePGN130312(NMsg, SID, TI, TS, ATemp, STemp);
 		
 	}
 	else if (NMsg.PGN == 130314)
 	{
 		uint8_t SID, PI, PS;
 		double P;
-		ParsePGN130314(NMsg, SID, PI, PS, P);
+		parsePGN130314(NMsg, SID, PI, PS, P);
 		
 	}
 }
@@ -78,7 +78,7 @@ void CANWindsensorNode::parsePGN130306(N2kMsg &NMsg, uint8_t &SID, float &WindSp
 	Reference = NMsg.Data[5] & 0x07;
 }
 
-void CANWindsensorNode::parsePGN130311(N2kMsg &Msg, uint8_t &SID, uint8_t &TemperatureInstance,	//Environmental Parameters
+void CANWindsensorNode::parsePGN130311(N2kMsg &NMsg, uint8_t &SID, uint8_t &TemperatureInstance,	//Environmental Parameters
 	uint8_t &HumidityInstance, float &Temperature,
 	float &Humidity, float &AtmosphericPressure)
 {
@@ -94,7 +94,7 @@ void CANWindsensorNode::parsePGN130311(N2kMsg &Msg, uint8_t &SID, uint8_t &Tempe
 	AtmosphericPressure = tmp;		//hPa
 }
 
-void CANWindsensorNode::ParsePGN130312(N2kMsg &NMsg, uint8_t &SID, uint8_t &TemperatureInstance,	//Temperature
+void CANWindsensorNode::parsePGN130312(N2kMsg &NMsg, uint8_t &SID, uint8_t &TemperatureInstance,	//Temperature
 					uint8_t &TemperatureSource, float &ActualTemperature,
 					float &SetTemperature)
 {
@@ -107,7 +107,7 @@ void CANWindsensorNode::ParsePGN130312(N2kMsg &NMsg, uint8_t &SID, uint8_t &Temp
 	SetTemperature = tmp*0.01;
 }
 
-void CANWindsensorNode::ParsePGN130314(N2kMsg &NMsg, uint8_t &SID, uint8_t &PressureInstance,		//ActualPressure
+void CANWindsensorNode::parsePGN130314(N2kMsg &NMsg, uint8_t &SID, uint8_t &PressureInstance,		//ActualPressure
 					uint8_t &PressureSource, double &Pressure)
 {
 	SID = NMsg.Data[0];
