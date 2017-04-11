5717eb5e7ec58f817d6962eeff5a044cc696246f
Martins A <jaydox9@yahoo.se>
2017-04-11 08:08:31 +0300

    Refactored so it compiles

--- Messages/StateMessage.h
+++ Messages/StateMessage.h
@@ -17,56 +17,36 @@
 
 class StateMessage : public Message {
 public:
-  StateMessage (NodeID destinationID, NodeID sourceID, int compassHeading, int compassPitch,
-    int compassRoll, bool gpsFix, bool gpsOnline, double lat, double lon, double unixTime,
-    double gpsSpeed, int gpsSatellite)
+  StateMessage (NodeID destinationID, NodeID sourceID, float compassHeading,
+    double lat, double lon, double gpsSpeed)
     :Message(MessageType::StateMessage, sourceID, destinationID),
-    m_CompassHeading(compassHeading), m_CompassPitch(compassPitch), m_CompassRoll(compassRoll),
-    m_GPSHasFix(gpsFix), m_GPSOnline(gpsOnline), m_GPSLat(lat), m_GPSLon(lon), m_GPSUnixTime(unixTime), m_GPSSpeed(gpsSpeed),
-    m_GPSHeading(heading), m_GPSSatellite(gpsSatellite)
+    vesselHeading(compassHeading), vesselLat(lat), vesselLan(lon), vesselSpeed(gpsSpeed)
     { }
 
-    StateMessage(	int compassHeading, int compassPitch, int compassRoll, bool gpsFix, bool gpsOnline, double lat,
-      double lon, double unixTime, double gpsSpeed, int gpsSatellite)
+
+    StateMessage(float compassHeading, double lat, double lon, double gpsSpeed)
       :Message(MessageType::VesselState, NodeID::None, NodeID::None),
-      m_CompassHeading(compassHeading), m_CompassPitch(compassPitch), m_CompassRoll(compassRoll),
-      m_GPSHasFix(gpsFix), m_GPSOnline(gpsOnline), m_GPSLat(lat), m_GPSLon(lon), m_GPSUnixTime(unixTime), m_GPSSpeed(gpsSpeed),
-      m_GPSHeading(heading), m_GPSSatellite(gpsSatellite)
+      vesselHeading(compassHeading), vesselLat(lat), vesselLan(lon), vesselSpeed(gpsSpeed)
       { }
+
+      float heading() { return vesselHeading; }
+      double latitude() { return vesselLat; }
+      double longitude() { return vesselLan; }
+      double speed() { return vesselSpeed; }
+
+      virtual ~StateMessage() { }
 
       StateMessage(MessageDeserialiser deserialiser)
       :Message(deserialiser)
       {
-        if(	!deserialiser.readInt(m_CompassHeading) ||
-        !deserialiser.readInt(m_CompassPitch) ||
-        !deserialiser.readInt(m_CompassRoll) ||
-        !deserialiser.readBool(m_GPSHasFix) ||
-        !deserialiser.readBool(m_GPSOnline) ||
-        !deserialiser.readDouble(m_GPSLat) ||
-        !deserialiser.readDouble(m_GPSLon) ||
-        !deserialiser.readDouble(m_GPSUnixTime) ||
-        !deserialiser.readDouble(m_GPSSpeed) ||
-        !deserialiser.readDouble(m_GPSHeading) ||
-        !deserialiser.readInt(m_GPSSatellite)
+        if(	!deserialiser.readFloat(vesselHeading) ||
+        !deserialiser.readDouble(vesselLat) ||
+        !deserialiser.readDouble(vesselLan) ||
+        !deserialiser.readDouble(vesselSpeed))
         {
           m_valid = false;
         }
       }
-
-      virtual ~StateMessage() { }
-
-      int compassHeading() { return m_CompassHeading; }
-      int compassPitch() { return m_CompassPitch; }
-      int compassRoll() { return m_CompassRoll; }
-
-      bool gpsHasFix() { return m_GPSHasFix; }
-      bool gpsOnline() { return m_GPSOnline; }
-      double latitude() { return m_GPSLat; }
-      double longitude() { return m_GPSLon; }
-      double unixTime() { return m_GPSUnixTime; }
-      double speed() { return m_GPSSpeed; }
-      double gpsHeading() { return m_GPSHeading; }
-      int gpsSatellite() { return m_GPSSatellite; }
 
       ///----------------------------------------------------------------------------------
       /// Serialises the message into a MessageSerialiser
@@ -75,29 +55,15 @@ public:
       {
         Message::Serialise(serialiser);
 
-        serialiser.serialise(m_CompassHeading);
-        serialiser.serialise(m_CompassPitch);
-        serialiser.serialise(m_CompassRoll);
-        serialiser.serialise(m_GPSHasFix);
-        serialiser.serialise(m_GPSOnline);
-        serialiser.serialise(m_GPSLat);
-        serialiser.serialise(m_GPSLon);
-        serialiser.serialise(m_GPSUnixTime);
-        serialiser.serialise(m_GPSSpeed);
-        serialiser.serialise(m_GPSHeading);
-        serialiser.serialise(m_GPSSatellite);
+        serialiser.serialise(vesselHeading);
+        serialiser.serialise(vesselLat);
+        serialiser.serialise(vesselLan);
+        serialiser.serialise(vesselSpeed);
       }
 
     private:
-      int 	m_CompassHeading;
-      int 	m_CompassPitch;
-      int 	m_CompassRoll;
-      bool	m_GPSHasFix;
-      bool	m_GPSOnline;
-      double	m_GPSLat;
-      double	m_GPSLon;
-      double	m_GPSUnixTime;
-      double	m_GPSSpeed;
-      double	m_GPSHeading;
-      int		m_GPSSatellite;
+      float 	vesselHeading;
+      double	vesselLat;
+      double	vesselLan;
+      double	vesselSpeed;
     };
