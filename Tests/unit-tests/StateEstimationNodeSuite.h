07e84cb3b38fee5011cebc196cb4b95d419e6a5d
Martins A <jaydox9@yahoo.se>
2017-04-18 09:45:32 +0300

    StateEstimationNode can send State messages to the message bus

---
+++ Tests/unit-tests/StateEstimationNodeSuite.h
@@ -0,0 +1,88 @@
+/****************************************************************************************
+*
+* File:
+* 		StateEstimationNodeSuite.h
+*
+* Purpose:
+*		A set of unit tests seeing if the StateEstimationNode works as intended
+*
+* Developer Notes:
+*
+*
+***************************************************************************************/
+
+#pragma once
+
+#include "Nodes/StateEstimationNode.h"
+#include "../cxxtest/cxxtest/TestSuite.h"
+#include "Messages/StateMessage.h"
+#include "../../MessageBus/MessageBus.h"
+#include "TestMocks/MessageLogger.h"
+#include "Messages/CompassDataMsg.h"
+#include "Messages/GPSDataMsg.h"
+
+
+// For std::this_thread
+#include <chrono>
+#include <thread>
+
+#define STATE_ESTIMATIONODE_TEST_COUNT   2
+#define SLEEP_TIME 20001
+
+
+class StateEstimationNodeSuite : public CxxTest::TestSuite
+{
+public:
+
+  StateEstimationNode* sEstimationNode;
+  std::thread* thr;
+  MessageLogger* logger;
+  int testCount = 0;
+  // Cheeky method for declaring and initialising a static in a header file
+  static MessageBus& msgBus(){
+    static MessageBus* mbus = new MessageBus();
+    return *mbus;
+  }
+
+  static void runMessageLoop()
+  {
+    msgBus().run();
+  }
+
+  void setUp()
+  {
+    // Only want to setup them up once in this test, only going to delete them when the program closes and the OS destroys
+    // the process's memory
+    if(sEstimationNode == 0)
+    {
+      Logger::DisableLogging();
+      logger = new MessageLogger(msgBus());
+      sEstimationNode = new StateEstimationNode(msgBus(), 0.5);
+      srand (time(NULL));
+      thr = new std::thread(runMessageLoop);
+    }
+    testCount++;
+    std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_FOR_MESSAGE));
+  }
+
+  void tearDown()
+  {
+    if(testCount == STATE_ESTIMATIONODE_TEST_COUNT)
+    {
+      delete sEstimationNode;
+      delete logger;
+    }
+  }
+
+  void test_StateEstimationNodeInit()
+  {
+    TS_ASSERT(sEstimationNode->init());
+  }
+
+  void test_StateEstimationNode(){
+    sEstimationNode->start();
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+    TS_ASSERT(logger->StateDataReceived());
+  }
+
+};
