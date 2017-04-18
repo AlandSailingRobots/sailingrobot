89f39277a4251d1c7a843b3aa6e8b891fdae6783
dalinel <eloise.dalin@gmail.com>
2017-04-18 10:18:21 +0300

    Some more changes to the integration test, keep getting error

--- Tests/integration/IntegrationTests/CANServiceIntegrationTest.cpp
+++ Tests/integration/IntegrationTests/CANServiceIntegrationTest.cpp
@@ -3,33 +3,37 @@
 #include "MessageBus/MessageBus.h"
 #include "Nodes/CANWindsensorNode.h"
 #include "Tests/integration/TestMocks/MessagePrinter.h"
+#include "Messages/WindDataMsg.h"
 
 #include <thread>
 #include <chrono>
 #include <future>
 
-#define WAIT_TIME 750
-
+#define WAIT_TIME 5
+/*
 static MessageBus& msgBus(){
   static MessageBus* mbus = new MessageBus();
   return *mbus;
 }
-
-void startMsgBus(){
-  msgBus().run();
+*/
+void startMsgBus(MessageBus& msgBus){
+  msgBus.run();
 }
 
 int main(int argc, char const *argv[]) {
 
   CANService service;
-
-  CANWindsensorNode windNode(msgBus(), service, 500);
-  MessagePrinter printer(msgBus());
+  MessageBus msgBus;
+  std::cout << &msgBus << std::endl;
+  CANWindsensorNode windNode(msgBus, service, 500);
+  MessagePrinter printer(msgBus);
 
   auto future = service.start();
   
   windNode.start();
-  std::async (std::launch::async, startMsgBus);
+  std::thread t1(startMsgBus, std::ref(msgBus));
+  t1.detach();
+  msgBus.sendMessage(std::make_unique<WindDataMsg>(1,2,3));
 
   std::this_thread::sleep_for(std::chrono::seconds(WAIT_TIME));
 
