160bb462106e1702e7b24c32153d8569db4d2406
jmagnuss <john.magnusson@ha.ax>
2017-05-03 10:34:15 +0300

    more debugging

--- HardwareServices/CAN_Services/CANFrameReceiver.cpp
+++ HardwareServices/CAN_Services/CANFrameReceiver.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 CANFrameReceiver::CANFrameReceiver(CANService& service, std::vector<uint32_t> IDs){
+  std::cout << "IDs empty ";
     for(uint32_t id : IDs) {
       std::cout << "in CANFrameReceiver ";
       service.registerForReading(*this, id);
