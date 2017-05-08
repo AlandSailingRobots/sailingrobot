8003c6a059157b9745118ccf295b36e6074015c4
periksso <philip.eriksson@protonmail.com>
2017-05-08 12:13:23 +0300

    passing integration test

--- Tests/integration/IntegrationTests/LowLevelControllerASPireIntegrationTest.cpp
+++ Tests/integration/IntegrationTests/LowLevelControllerASPireIntegrationTest.cpp
@@ -15,15 +15,18 @@ int main() {
     std::vector<uint32_t> IDs = { 700 };
     MockCANReceiver mock (canService, IDs);
 
-    canService.start();
+    auto f = canService.start();
     canService.SetLoopBackMode();
-    std::async(std::launch::async, &msgBusLoop);
-    
+    std::thread t (msgBusLoop);
+    t.detach();
+
     NavigationControlMsg::NavigationState state =
                     NavigationControlMsg::NavigationState::sailToWaypoint;
     msgBus.sendMessage(std::make_unique<NavigationControlMsg>(1.2, 2.3, false, state));
     msgBus.sendMessage(std::make_unique<StateMessage>(1,2,3,4,5));
     msgBus.sendMessage(std::make_unique<WindStateMsg>(6,7,8,9));
+    canService.stop();
+    f.get();
 
     std::cout << mock.message_received() << std::endl;
 
