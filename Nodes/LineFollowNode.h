7a4627d56d728f2bab6db95d2301649fc3d0c58a
Martins A <jaydox9@yahoo.se>
2017-05-09 14:01:21 +0300

    Added member variables from WindStateMsg and StateMessage to LineFollowNode to save the state of those messages

--- Nodes/LineFollowNode.h
+++ Nodes/LineFollowNode.h
@@ -1,19 +1,19 @@
- /****************************************************************************************
- *
- * File:
- * 		LineFollowNode.h
- *
- * Purpose:
- *		This class computes the actuator positions of the boat in order to follow
- *    lines given by the waypoints.
- *
- * Developer Notes: algorithm inspired and modified from Luc Jaulin and
- *    Fabrice Le Bars  "An Experimental Validation of a Robust Controller with the VAIMOS
- *    Autonomous Sailboat" and "Modeling and Control for an Autonomous Sailboat: A
- *    Case Study" from Jon Melin, Kjell Dahl and Matia Waller
- *
- *
- ***************************************************************************************/
+/****************************************************************************************
+*
+* File:
+* 		LineFollowNode.h
+*
+* Purpose:
+*		This class computes the actuator positions of the boat in order to follow
+*    lines given by the waypoints.
+*
+* Developer Notes: algorithm inspired and modified from Luc Jaulin and
+*    Fabrice Le Bars  "An Experimental Validation of a Robust Controller with the VAIMOS
+*    Autonomous Sailboat" and "Modeling and Control for an Autonomous Sailboat: A
+*    Case Study" from Jon Melin, Kjell Dahl and Matia Waller
+*
+*
+***************************************************************************************/
 
 #pragma once
 
@@ -47,8 +47,7 @@ private:
 	double 	m_nextWaypointLat;
 	int 	m_nextWaypointDeclination;
 	int 	m_nextWaypointRadius;
-
-    int 	m_prevWaypointId;
+	int 	m_prevWaypointId;
 	double 	m_prevWaypointLon;
 	double 	m_prevWaypointLat;
 	int 	m_prevWaypointDeclination;
@@ -56,10 +55,25 @@ private:
 
 	bool 	m_externalControlActive;
 
-    bool    m_tack;
-    double  m_maxCommandAngle, m_maxSailAngle, m_minSailAngle;
-    double  m_tackAngle;
-    int     m_tackingDirection;
+	bool    m_tack;
+	double  m_maxCommandAngle, m_maxSailAngle, m_minSailAngle;
+	double  m_tackAngle;
+	int     m_tackingDirection;
+
+	//same as compass heading
+	float 	m_VesselHeading;
+
+	double	m_VesselLat;
+	double	m_VesselLon;
+	double	m_VesselSpeed;
+
+	//same as the gpsheading
+	double  m_VesselCourse;
+
+	double m_trueWindSpeed;
+	double m_trueWindDir;
+	double m_apparentWindSpeed;
+	double m_apparentWindDir;
 
 	RudderCommand m_rudderCommand;
 	SailCommand m_sailCommand;
@@ -67,18 +81,18 @@ private:
 	std::vector<float> twdBuffer;
 	unsigned int twdBufferMaxSize;
 
-    double calculateAngleOfDesiredTrajectory(VesselStateMsg* msg);
+	double calculateAngleOfDesiredTrajectory(VesselStateMsg* msg);
 	void calculateActuatorPos(VesselStateMsg* msg);
-    void setPrevWaypointData(WaypointDataMsg* waypMsg, VesselStateMsg* vesselMsg);
+	void setPrevWaypointData(WaypointDataMsg* waypMsg, VesselStateMsg* vesselMsg);
 
 	virtual int getHeading(int gpsHeading, int compassHeading, double gpsSpeed, bool mockPosition, bool getHeadingFromCompass);
-  //Calculates a smooth transition between the compass and the gps. Do not call directly, use getHeading()
+	//Calculates a smooth transition between the compass and the gps. Do not call directly, use getHeading()
 	int getMergedHeading(int gpsHeading, int compassHeading, bool increaseCompassWeight);
 	void setupRudderCommand();
 	void setupSailCommand();
-    bool getGoingStarboard();
+	bool getGoingStarboard();
 	void setPrevWaypointToBoatPos(VesselStateMsg* msg);
 
 	/*void manageDatabase(VesselStateMsg* msg, double trueWindDirection, double rudder, double sail, double heading,
-                        double distanceToNextWaypoint, double bearingToNextWaypoint);*/
+	double distanceToNextWaypoint, double bearingToNextWaypoint);*/
 };
