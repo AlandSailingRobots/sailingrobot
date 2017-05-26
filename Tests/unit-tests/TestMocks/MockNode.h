f091c7712a6e7758c1b50fcc4e48d9ac8e93e794
Martins A <jaydox9@yahoo.se>
2017-05-26 11:09:37 +0300

    Extended with new variables

--- Tests/unit-tests/TestMocks/MockNode.h
+++ Tests/unit-tests/TestMocks/MockNode.h
@@ -1,15 +1,15 @@
 /****************************************************************************************
- *
- * File:
- * 		MockNode.h
- *
- * Purpose:
- *		A mock node for testing whether message passing is working correctly.
- *
- * Developer Notes:
- *
- *
- ***************************************************************************************/
+*
+* File:
+* 		MockNode.h
+*
+* Purpose:
+*		A mock node for testing whether message passing is working correctly.
+*
+* Developer Notes:
+*
+*
+***************************************************************************************/
 
 
 #pragma once
@@ -18,14 +18,18 @@
 #include "../../../Nodes/Node.h"
 #include "../../../Messages/GPSDataMsg.h"
 #include "../../../Messages/WindDataMsg.h"
+#include "../../../Messages/CompassDataMsg.h"
+
 
 
 class MockNode : Node {
 public:
 	MockNode(MessageBus& msgBus, bool& registered)
-		:Node(NodeID::MessageLogger, msgBus), m_MessageReceived(false), m_HasFix(false), m_Online(false), m_WindDir(0), m_WindSpeed(0)
+	:Node(NodeID::MessageLogger, msgBus), m_MessageReceived(false), m_HasFix(false),
+	m_Online(false), m_WindDir(0), m_WindSpeed(0)
 	{
-		if(msgBus.registerNode(*this, MessageType::GPSData) && msgBus.registerNode(*this, MessageType::WindData))
+		if(msgBus.registerNode(*this, MessageType::GPSData) && msgBus.registerNode(*this, MessageType::WindData) &&
+			msgBus.registerNode(*this, MessageType::CompassData))
 		{
 			registered = true;
 		}
@@ -50,8 +54,10 @@ public:
 				m_Online = gpsMsg->gpsOnline();
 				m_Lat = gpsMsg->latitude();
 				m_Lon = gpsMsg->longitude();
+				m_Speed = gpsMsg->speed();
+				m_Course = gpsMsg->heading();
 			}
-				break;
+			break;
 			case MessageType::WindData:
 			{
 				m_MessageReceived = true;
@@ -60,9 +66,16 @@ public:
 				m_WindSpeed = windMsg->windSpeed();
 				m_WindTemp = windMsg->windTemp();
 			}
-				break;
+			break;
+
+			case MessageType::CompassData:
+			{
+				m_MessageReceived = true;
+				CompassDataMsg* compassMsg = (CompassDataMsg*)message;
+				m_Heading = compassMsg->heading();
+			}
 			default:
-				return;
+			return;
 		}
 	}
 
@@ -72,9 +85,12 @@ public:
 	bool	m_Online;
 	double 	m_Lat;
 	double 	m_Lon;
+	double m_Speed;
+	double m_Course;
 
 	float 	m_WindDir;
 	float 	m_WindSpeed;
 	float 	m_WindTemp;
-};
+	float 	m_Heading;
 
+};
