a8cd5b997519ad4e2a2b9ab8fe6be06e6b5b2790
Martins A <jaydox9@yahoo.se>
2017-05-30 14:33:15 +0300

    Testsuite refactored

--- Tests/unit-tests/LineFollowSuite.h
+++ Tests/unit-tests/LineFollowSuite.h
@@ -28,7 +28,6 @@
 
 #include "../cxxtest/cxxtest/TestSuite.h"
 #include "../../MessageBus/MessageBus.h"
-#include "TestMocks/MessageLogger.h"
 #include "Nodes/LineFollowNode.h"
 #include "Messages/VesselStateMsg.h"
 #include "Messages/WindStateMsg.h"
@@ -49,7 +48,10 @@ class LineFollowSuite : public CxxTest::TestSuite {
 public:
   LineFollowNode* lineFollow;
   std::thread* thr;
-  MessageLogger* logger;
+
+  MockNode* mockNode;
+  bool nodeRegistered = false;
+
   int testCount = 0;
   DBHandler* dbHandler;
 
@@ -73,10 +75,11 @@ public:
     {
       dbHandler = new DBHandler("./asr.db");
       Logger::DisableLogging();
-      logger = new MessageLogger(msgBus());
       lineFollow = new LineFollowNode(msgBus(), *dbHandler);
+
       thr = new std::thread(runMessageLoop);
     }
+    mockNode = new MockNode(msgBus(), nodeRegistered);
     testCount++;
     std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_FOR_MESSAGE));
   }
@@ -86,14 +89,16 @@ public:
     if(testCount == LINEFOLLOW_TEST_COUNT)
     {
       delete lineFollow;
-      delete logger;
       delete dbHandler;
     }
+    delete mockNode;
+
   }
 
   void test_LineFollowInit()
   {
     TS_ASSERT(lineFollow->init());
+    TS_ASSERT(nodeRegistered);
   }
 
   void test_LineFollowCalculateActuatorPosition()
@@ -107,41 +112,8 @@ public:
 
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     TS_TRACE("END OF LINEFOLLOW");
-    TS_ASSERT(logger->navigationDataReceived());
+    TS_ASSERT(mockNode->m_MessageReceived);
   }
 
-  void test_LineFollowCalculateAngleOfDesiredTrajectory()
-  {
-    MessagePtr msg = std::make_unique<WindStateMsg>(170, 30, 0, 200);
-    msgBus().sendMessage(std::move(msg));
-    MessagePtr sMsg = std::make_unique<StateMessage>(170, 360, 10, 200, 50);
-    msgBus().sendMessage(std::move(sMsg));
-    MessagePtr wMsg = std::make_unique<WaypointDataMsg>(1, 50, 90, 0, 6, 15,
-      5, 40, 90, 6, 15);
-    msgBus().sendMessage(std::move(wMsg));
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-
-      int m_tackAngle = 0.872665; //50°
-
-      double trueWindDirection_radian = Utility::degreeToRadian(30)+M_PI;
-
-      double signedDistance = Utility::calculateSignedDistanceToLine(50, 90, 40, 5, 10, 360);
-      int maxTackDistance = 40; //'r'
-
-      double desiredHeading = (2 * (M_PI / 4)/M_PI) * atan(signedDistance/maxTackDistance); //heading to smoothly join the line
-
-      int m_tackingDirection = 1;
-      //---------------------
-      //Change tacking direction when reaching max distance
-      if(abs(signedDistance) > maxTackDistance)
-      {
-        m_tackingDirection = -Utility::sgn(signedDistance);
-      }
-
-      desiredHeading = M_PI + trueWindDirection_radian - m_tackingDirection * m_tackAngle;/* sail around the wind direction */
-      desiredHeading = Utility::limitRadianAngleRange(desiredHeading);
-
-      TS_ASSERT(desiredHeading != 0);
-    }
 
   };
