ccecca4986a2d47ae344cc517d392c97c7643c78
legalmael <legal.mael@hotmail.fr>
2017-06-21 16:01:52 +0300

    Creat NavigationSystem folder

---
+++ NavigationSystem/line-follow.mk
@@ -0,0 +1,52 @@
+###############################################################################
+#
+# Line Follower Development Makefile
+#
+###############################################################################
+
+
+###############################################################################
+# Files
+###############################################################################
+
+
+SRC         = $(MAIN_SRC) $(CORE_SRC) $(LINE_FOLLOW_SRC) $(HTTP_SYNC_SRC)
+
+ifeq ($(USE_SIM),1)
+
+SRC         += $(SIMULATOR_SRC)
+
+else
+
+SRC         += $(HARDWARE_NODES_SRC) $(HARDWARE_SERVICES_SRC) $(XBEE_NETWORK_SRC)
+
+endif
+
+# Object files
+OBJECTS = $(addprefix $(BUILD_DIR)/, $(SRC:.cpp=.o))
+
+
+###############################################################################
+# Rules
+###############################################################################
+
+
+all: $(EXECUTABLE) stats
+
+# Link and build
+$(EXEC_DIR)/$(EXECUTABLE): $(OBJECTS)
+	rm -f $(OBJECT_FILE)
+	@echo -n " " $(OBJECTS) >> $(OBJECT_FILE)
+	@echo Linking object files
+	$(CXX) $(LDFLAGS) @$(OBJECT_FILE) -Wl,-rpath=./ -o $@ $(LIBS)
+
+# Compile CPP files into the build folder
+$(BUILD_DIR)/%.o:$(SRC_DIR)/%.cpp
+	@mkdir -p $(dir $@)
+	@echo Compiling CPP File: $@
+
+	@$(CXX) -c $(CPPFLAGS) $(INC_DIR) -o ./$@ $< $(DEFINES) $(LIBS)
+
+stats:$(EXECUTABLE)
+	@echo Final executable size:
+	$(SIZE) $(EXECUTABLE)
