679653f4da4e1ce66327c74f8ea6898bf90503d4
legalmael <legal.mael@hotmail.fr>
2017-08-01 15:33:31 +0300

    small changes in CANArduinoNode

--- NavigationSystem/Hardwares/CANArduinoNode.cpp
+++ NavigationSystem/Hardwares/CANArduinoNode.cpp
@@ -13,17 +13,10 @@
 ***************************************************************************************/
 
 #include "CANArduinoNode.h"
-#include "Messages/ArduinoDataMsg.h"
-#include "Messages/ASPireActuatorFeedbackMsg.h"
-#include "SystemServices/Timer.h"
-#include "Math/Utility.h"
 
-#include <chrono>
-#include <thread>
 
 CANArduinoNode::CANArduinoNode(MessageBus& messageBus, CANService& canService, int time_filter_ms) :
 ActiveNode(NodeID::CANArduino, messageBus), CANFrameReceiver(canService, {701,702}), m_loopTime (time_filter_ms)
-
 {
 	m_RudderFeedback  = DATA_OUT_OF_RANGE;
 	m_WingsailFeedback = DATA_OUT_OF_RANGE;
@@ -50,12 +43,12 @@ void CANArduinoNode::processFrame (CanMsg& msg) {
 	if (msg.id == 701) {
 		rawData = (msg.data[1] << 8 | msg.data[0]);		 
 		m_RudderFeedback = Utility::mapInterval (rawData, 0, INT16_SIZE, -MAX_RUDDER_ANGLE, MAX_RUDDER_ANGLE);
-		rawData = (msg.data[3] << 8 | msg.data[2]);
 
+		rawData = (msg.data[3] << 8 | msg.data[2]);
 		m_WingsailFeedback = Utility::mapInterval (rawData, 0, INT16_SIZE, -MAX_WINGSAIL_ANGLE, MAX_WINGSAIL_ANGLE);
+
 		m_WindvaneSelfSteerAngle = (msg.data[5] << 8 | msg.data[4]);
 		m_WindvaneActuatorPos = msg.data[7];
-
 
 	} else if (msg.id == 702) {
 		m_Radio_Controller_On = (msg.data[1] << 8 | msg.data[0]);
@@ -82,17 +75,18 @@ void CANArduinoNode::CANArduinoNodeThreadFunc(ActiveNode* nodePtr) {
 			node->m_lock.unlock();
 			continue;
 		}
-	MessagePtr feebackData = std::make_unique<ASPireActuatorFeedbackMsg>( node->m_WingsailFeedback, node->m_RudderFeedback,
-																	node->m_WindvaneSelfSteerAngle, node->m_WindvaneActuatorPos);
-	node->m_MsgBus.sendMessage(std::move(feebackData));
-	
-	MessagePtr statusMsg = std::make_unique<ArduinoDataMsg>(0,0,0,0,node->m_Radio_Controller_On);
-	node->m_MsgBus.sendMessage(std::move(statusMsg));
+		MessagePtr feebackData = std::make_unique<ASPireActuatorFeedbackMsg>( node->m_WingsailFeedback, node->m_RudderFeedback,
+																		node->m_WindvaneSelfSteerAngle, node->m_WindvaneActuatorPos);
+		node->m_MsgBus.sendMessage(std::move(feebackData));
+		
+		MessagePtr statusMsg = std::make_unique<ArduinoDataMsg>(0,0,0,0,node->m_Radio_Controller_On);
+		node->m_MsgBus.sendMessage(std::move(statusMsg));
 
-	node->m_lock.unlock();
-	// Need to convert milliseconds into seconds for the argument
-	timer.sleepUntil(node->m_loopTime*1.0f / 1000);
-	timer.reset();
+		node->m_lock.unlock();
+
+		// Need to convert milliseconds into seconds for the argument
+		timer.sleepUntil(node->m_loopTime*1.0f / 1000);
+		timer.reset();
 	}
 }
 
--- NavigationSystem/Hardwares/CANArduinoNode.h
+++ NavigationSystem/Hardwares/CANArduinoNode.h
@@ -1,15 +1,32 @@
+/****************************************************************************************
+*
+* File:
+* 		CANArduinoNode.h
+*
+* Purpose:
+*		 Process messages from the arduino in the Actuatorunit via the CAN-Service including ActuatorFeedback and RC status . 
+*
+* Developer Notes:
+*		 The CAN frame id numbers that this node subscribes to are:
+*			701, 702, (more to be added later?)
+*
+***************************************************************************************/
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <iostream>
+
 #include "Hardwares/CAN_Services/CANService.h"
 #include "Hardwares/CAN_Services/CANFrameReceiver.h"
 #include "Hardwares/CAN_Services/N2kMsg.h"
 #include "MessageBus/ActiveNode.h"
 #include "MessageBus/Message.h"
 #include "MessageBus/MessageBus.h"
+#include "Messages/ArduinoDataMsg.h"
+#include "Messages/ASPireActuatorFeedbackMsg.h"
 #include "SystemServices/Timer.h"
 #include "Math/Utility.h"
-
-#include <mutex>
-#include <vector>
-#include <iostream>
 
 
 class CANArduinoNode : public ActiveNode, public CANFrameReceiver {
@@ -26,7 +43,7 @@ private:
 	
 	static void CANArduinoNodeThreadFunc(ActiveNode* nodePtr);
 
-	const int DATA_OUT_OF_RANGE	=	-2000;
+	const int DATA_OUT_OF_RANGE	= -2000;
 	const float MAX_RUDDER_ANGLE = 30;
 	const float MAX_WINGSAIL_ANGLE = 13;
 	const float INT16_SIZE = 65535;
