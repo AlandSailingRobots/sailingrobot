813aa1034cbb4f74800f0978abfe738f634f20c4
ClementRolinat <clement.rolinat@ensta-bretagne.org>
2017-09-07 15:25:59 +0200

    fix camera

--- NavigationSystem/WorldState/obstacledetection/CameraProcessingNode.cpp
+++ NavigationSystem/WorldState/obstacledetection/CameraProcessingNode.cpp
@@ -44,7 +44,7 @@ using namespace cv;
 vector<Obstacle> obstacle_list;
  
 CameraProcessingNode::CameraProcessingNode(MessageBus& msgBus, CollidableMgr& collidableMgr)
-  : ActiveNode(NodeID::CameraProcessingNode, msgBus), m_LoopTime(0.5), collidableMgr(collidableMgr) {
+  : ActiveNode(NodeID::CameraProcessingNode, msgBus), m_LoopTime(0.5), collidableMgr(&collidableMgr) {
     msgBus.registerNode(*this, MessageType::CompassData);
   }
 
