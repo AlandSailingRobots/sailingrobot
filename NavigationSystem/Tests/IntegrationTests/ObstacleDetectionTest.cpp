217d9fe63cada5bcaeee7a975850ecba49e4118b
annafriebe <anna.friebe@ha.ax>
2017-11-13 13:55:52 +0200

    testing to retrieve the roi where the thermal image is

--- NavigationSystem/Tests/IntegrationTests/ObstacleDetectionTest.cpp
+++ NavigationSystem/Tests/IntegrationTests/ObstacleDetectionTest.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************************
- * 
+ *
  * File:
  * 		ObstacleDetectionTest.cpp
  *
@@ -40,6 +40,11 @@ using namespace cv;
 #define DETECTOR_LOOP_TIME 250 // in ms (250 * 20 ms = 5s)
 #define MAX_COMPASS_FRAME_TIMEFRAME 10 // in ms
 
+const int lowFrameX = 0; //65;
+const int highFrameX = 640;  //505;
+const int lowFrameY = 0;  //97;
+const int highFrameY = 480;  //447;
+
 //DBHandler dbHandler("../asr.db");
 //MessageBus msgBus;
 //CollidableMgr cMgr;
@@ -47,65 +52,68 @@ struct Compass
 {
     float roll = 0;
     float heading = 0;
-    int tmsp = 0; 
+    int tmsp = 0;
 } m_compass_data;
 
-int CAMERA_APERTURE_X = 320, CAMERA_APERTURE_Y = 240;
+//int CAMERA_APERTURE_X = 320, CAMERA_APERTURE_Y = 240;
 
 //void messageLoop() {
 //    msgBus.run();
 //}
 
-int main() 
+int main()
 {
     Logger::init("ObstacleDetectionTest.log");
-    
+
 //    cMgr.startGC();
-    
+
 //    std::thread thr(messageLoop);
 //    thr.detach();
 //    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    
+
     VideoCapture m_capture(0); // Opens the camera handle
     if (m_capture.isOpened() == false) //  To check if object was associated to webcam successfully
     {
         Logger::error("Webcam not available");
         return -1;
-    } 
-    
-    // Set frame size
-    m_capture.set(CV_CAP_PROP_FRAME_WIDTH, CAMERA_APERTURE_X);
-    m_capture.set(CV_CAP_PROP_FRAME_HEIGHT, CAMERA_APERTURE_Y);
-    
+    }
+
+    // Set frame size - this appears not to work
+    //m_capture.set(CV_CAP_PROP_FRAME_WIDTH, CAMERA_APERTURE_X);
+    //m_capture.set(CV_CAP_PROP_FRAME_HEIGHT, CAMERA_APERTURE_Y);
+
+    Mat imgFullSize; // Input raw image
     Mat imgOriginal; // Input raw image
     Mat hsvImg; // HSV converted image
     // Image containers
     Mat frameGrayScale, roi, dst, cdst;
-    
+
     // Noise removal kernel for the filters
     Mat kernel_ero = getStructuringElement(MORPH_RECT, Size(2,2));
-    
+
     // Save detected lines
     std::vector<Vec4i> lines;
     std::vector<Point> locations;   // output, locations of non-zero pixels
-    
+
     // For green dot detection
     std::vector<Vec3f> circles;
-    
+
     // Tilt correction
     Mat rot;
     Rect imageBox;
-    
+
     // Set up frame size
-    m_capture >> imgOriginal;
+    m_capture >> imgFullSize;
+    Rect thermalImagerArea(lowFrameX, lowFrameY, widthFrameX, heightFrameY);
+    imgOriginal = roi(Rect, imgFullSize);
     Point2f center(imgOriginal.cols/2.0, imgOriginal.rows/2.0);
-    
+
     // frame size
-    double fWidth = m_capture.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-    double fHeight = m_capture.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+//    double fWidth = m_capture.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+//    double fHeight = m_capture.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
     // float cameraAngleApertureXPerPixel = CAMERA_APERTURE_X/fWidth;
     // float cameraAngleApertureYPerPixel = CAMERA_APERTURE_Y/fHeight;
-    
+
     // Create windows
  //   std::string find_lines_window = "Found Lines";
  //   namedWindow(find_lines_window, WINDOW_AUTOSIZE);
@@ -113,16 +121,16 @@ int main()
  //   namedWindow(roi_window, WINDOW_AUTOSIZE);
  //   std::string side_fill_window = "Side Fill";
  //   namedWindow(side_fill_window, WINDOW_AUTOSIZE);
-    
-    for(;;) 
+
+    for(;;)
     {
         m_capture >> imgOriginal;
-        
+
         if (imgOriginal.empty()) { // if frame read unsuccessfully
             Logger::error("video input frame not readable");
             break;
         }
-        
+
         /*
          * -----------------------------------------------------------------
          * Correct tilted image
@@ -130,7 +138,7 @@ int main()
          */
         // Check if compass data is up to date
         if(SysClock::unixTime() - m_compass_data.tmsp < MAX_COMPASS_FRAME_TIMEFRAME)
-        {            
+        {
             // Create the rotation matrix
             rot = getRotationMatrix2D(center, m_compass_data.roll, 1.0);
             // Determine bounding rectangle
@@ -141,37 +149,37 @@ int main()
             // Perform rotation
             warpAffine(imgOriginal, imgOriginal, rot, imageBox.size());
         }
-        
+
         /*
          * -----------------------------------------------------------------
          * Convert image to HSV color space
          *-----------------------------------------------------------------
          */
         // Convert Original Image to HSV Thresh Image
-        cvtColor(imgOriginal, hsvImg, CV_BGR2HSV); 
-        
-        /* 
+        cvtColor(imgOriginal, hsvImg, CV_BGR2HSV);
+
+        /*
          * -----------------------------------------------------------------
          * Find green circle indicating camera recalibration
          *-----------------------------------------------------------------
          */
         // Find green pixels
         inRange(hsvImg, Scalar(45, 100, 100), Scalar(75, 255, 255), hsvImg);
-        
+
         // Avoid false positives with a general blur
         GaussianBlur(hsvImg, hsvImg, Size(9, 9), 2, 2);
-        
+
         // Find circles
         HoughCircles(hsvImg, circles, CV_HOUGH_GRADIENT, 1, hsvImg.rows/8, 85, 15, 0, 0);
-        
-        if(circles.size() != 0) 
+
+        if(circles.size() != 0)
         {
             // A green circle has been detected, he thermal camera is recalibrating and frames may be unreliable and corrupted, sleep thread
             Logger::info("Camera is recalibrating, thread will sleep for 3 seconds");
-            std::this_thread::sleep_for(3s); 
+            std::this_thread::sleep_for(3s);
             continue;
         }
-        
+
         /*
          * -----------------------------------------------------------------
          * Denoising
@@ -180,17 +188,17 @@ int main()
         /** @todo Tune noise elimination filters parameters */
         //Convert image to grayscale
         cvtColor( imgOriginal, frameGrayScale, CV_BGR2GRAY );
-        
+
         // Apply a Gaussian Filter to clear out general noise
         GaussianBlur( frameGrayScale, frameGrayScale, Size(7, 7), 2.0, 2.0 );
-        
+
         // Remove spot noise
         medianBlur(frameGrayScale, frameGrayScale, 3);
-        
+
         // Remove small objects
         erode( frameGrayScale, frameGrayScale, kernel_ero );
         dilate( frameGrayScale, frameGrayScale, kernel_ero );
-        
+
         /*
          * -----------------------------------------------------------------
          * Find the horizon and set up the ROI (region of interest)
@@ -199,56 +207,56 @@ int main()
          */
         Canny(frameGrayScale, dst, 20, 50, 3);
         cvtColor(dst, cdst, COLOR_GRAY2BGR);
-        
+
         HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
         double theta1, theta2, hyp;
-        
+
         // Horizon
         Vec4i max_l;
         double max_dist = -1.0;
-        
+
         for( size_t i = 0; i < lines.size(); i++ )
         {
             Vec4i l = lines[i];
             theta1 = (l[3]-l[1]);
             theta2 = (l[2]-l[0]);
             hyp = hypot(theta1,theta2);
-            
+
             Point p1, p2;
             p1=Point(l[0], l[1]);
             p2=Point(l[2], l[3]);
-            
+
             //calculate angle in degrees
             float angle = atan2(p1.y - p2.y, p1.x - p2.x)*180/CV_PI;
-            
+
             // +/- 45 deg max inclination and min 1/3 of the frame width size (tilt correction may not always work)
             if(angle < 135 || angle > 225 || hyp < fWidth/3)
             {
                 lines.erase(lines.begin() + i);
                 continue;
             }
-            
+
             // select the greatest line only
-            if (max_dist < hyp) 
+            if (max_dist < hyp)
             {
                 max_l = l;
                 max_dist = hyp;
             }
         }
-        
+
         // show lines found
         line( cdst, Point(max_l[0], max_l[1]), Point(max_l[2], max_l[3]), Scalar(255,0,0), 3, LINE_AA);
 //        imshow( find_lines_window , cdst );
         imwrite("findLinesImg.jpg", cdst);
 //        waitKey(30);
-        
+
         /*
          * -----------------------------------------------------------------
          * Define ROI (Region of Interest)
          *-----------------------------------------------------------------
          */
         Rect rect(Point(0, max_l[1]), Point(fWidth,fHeight));
-        
+
         if( rect.area() > 0 )
         {
             roi = dst(rect);
@@ -258,59 +266,59 @@ int main()
         {
             roi = dst;
         }
-    
+
 //        imshow( roi_window, roi );
        imwrite("roiImg1.jpg", roi);
 //        waitKey(30);
-        
+
         /*
          * -----------------------------------------------------------------
          * Colour the frame in B/W according to lines detected
          *-----------------------------------------------------------------
          */
-        for (int j = roi.cols-1; j>=0; j--) 
+        for (int j = roi.cols-1; j>=0; j--)
         {
             bool white = true;
-            for (int i = roi.rows-1; i>=0; i--) 
+            for (int i = roi.rows-1; i>=0; i--)
             {
                 if (roi.at<unsigned char>(i,j) > 0)
                     white = false;
-                
+
                 if(white)
                     roi.at<unsigned char>(i,j)=255;
                 else
                     roi.at<unsigned char>(i,j)=0;
             }
         }
-        
+
 //        imshow( side_fill_window, roi );
 //        waitKey(30);
         imwrite("sideFillImg.jpg", roi);
-        
+
         /*
          * -----------------------------------------------------------------
          * Colour the frame in B/W according to lines detected
          *-----------------------------------------------------------------
          */
-        for (int col = roi.cols-1; col>=0; col--) 
+        for (int col = roi.cols-1; col>=0; col--)
         {
             int row = roi.rows-1;
             do{
                 row--;
             }while(roi.at<unsigned char>(row,col) != 255);
-            
+
             // float bearing = col*webcamAngleApertureXPerPixel m_compass_data.heading;
-            
+
             // collidableMgr->addVisualObstacle(row, bearing);
         }
-        
+
 //        imshow( roi_window, roi );
 //        waitKey(30);
         imwrite("roiImg2.jpg", roi);
-        
+
     }
-    
+
     Logger::error("exited loop");
-    
+
     return 0;
 }
