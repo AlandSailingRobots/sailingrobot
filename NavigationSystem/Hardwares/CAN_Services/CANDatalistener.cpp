8ad534c92b84d668a60f32742b18815e8925d7a5
Charlotta Solax <charlotta.solax@ha.ax>
2018-04-04 14:27:09 +0300

    copied files wherefrom CANDatalistener inherit

---
+++ NavigationSystem/Hardwares/CAN_Services/CANDatalistener.cpp
@@ -0,0 +1,15 @@
+#include "CANDatalistner.h"
+#include "CANService.h"
+
+#include <iostream>
+
+/*CANDatalistner::CANDatalistner(CANService& service, std::vector<uint32_t> IDs){
+    for(uint32_t id : IDs) {
+      service.registerForReading(*this, id);
+    }
+}
+
+CANDatalistner::CANDatalistner(CANService& service, uint32_t ID) {
+  service.registerForReading(*this, ID);
+}
+*/
---
+++ NavigationSystem/Hardwares/CAN_Services/CANDatalistener.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class CANService;
+
+#include "N2kMsg.h"
+
+#include <stdint.h>
+#include <vector>
+/*
+class CANDatalistner
+{
+
+public:
+	CANDatalistner(CANService& service, std::vector<uint32_t> IDs);
+	CANDatalistner(CANService& service, uint32_t ID);
+
+	virtual void processFrame(CanMsg& msg) = 0;
+};
+*/
