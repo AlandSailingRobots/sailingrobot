006664630b913eac90d6b34ffb026190e97ba579
Emma Brandt <emma.brandt@ha.ax>
2018-04-10 15:44:12 +0300

    changed variable names to not include next keyword

--- NavigationSystem/Messages/WaypointDataMsg.h
+++ NavigationSystem/Messages/WaypointDataMsg.h
@@ -19,19 +19,19 @@
 class WaypointDataMsg : public Message {
 public:
 	WaypointDataMsg(NodeID destinationID, NodeID sourceID, int nextId, double nextLongitude, double nextLatitude, int nextDeclination, int nextRadius,
-                        int nextStayTime, double nextTimeInterval, bool nextMeasureAtCheckpoint, int prevId, double prevLongitude, double prevLatitude, int prevDeclination, int prevRadius)
+                        int nextStayTime, double timeInterval, bool measureAtCheckpoint, int prevId, double prevLongitude, double prevLatitude, int prevDeclination, int prevRadius)
 		:Message(MessageType::WaypointData, sourceID, destinationID), m_nextId(nextId), m_nextLongitude(nextLongitude), m_nextLatitude(nextLatitude),
                          m_nextDeclination(nextDeclination), m_nextRadius(nextRadius), m_nextStayTime(nextStayTime),
-						 m_nextTimeInterval(nextTimeInterval), m_nextMeasureAtCheckpoint(nextMeasureAtCheckpoint), m_prevId(prevId),
+						 m_timeInterval(timeInterval), m_measureAtCheckpoint(measureAtCheckpoint), m_prevId(prevId),
                          m_prevLongitude(prevLongitude), m_prevLatitude(prevLatitude), m_prevDeclination(prevDeclination), m_prevRadius(prevRadius)
 	{ }
 
 	WaypointDataMsg(int nextId, double nextLongitude, double nextLatitude, int nextDeclination, int nextRadius, int nextStayTime,
-						double nextTimeInterval, bool nextMeasureAtCheckpoint,
+						double timeInterval, bool measureAtCheckpoint,
                         int prevId, double prevLongitude, double prevLatitude, int prevDeclination, int prevRadius)
 		:Message(MessageType::WaypointData, NodeID::None, NodeID::None), m_nextId(nextId), m_nextLongitude(nextLongitude), m_nextLatitude(nextLatitude),
                          m_nextDeclination(nextDeclination), m_nextRadius(nextRadius), m_nextStayTime(nextStayTime),
-						 m_nextTimeInterval(nextTimeInterval), m_nextMeasureAtCheckpoint(nextMeasureAtCheckpoint), m_prevId(prevId), m_prevLongitude(prevLongitude),
+						 m_timeInterval(timeInterval), m_measureAtCheckpoint(measureAtCheckpoint), m_prevId(prevId), m_prevLongitude(prevLongitude),
                          m_prevLatitude(prevLatitude), m_prevDeclination(prevDeclination), m_prevRadius(prevRadius)
 
 	{ }
@@ -45,8 +45,8 @@ public:
 			!deserialiser.readInt(m_nextDeclination) ||
 			!deserialiser.readInt(m_nextRadius) ||
 			!deserialiser.readInt(m_nextStayTime) ||
-			!deserialiser.readDouble(m_nextTimeInterval) ||
-			!deserialiser.readBool(m_nextMeasureAtCheckpoint) ||
+			!deserialiser.readDouble(m_timeInterval) ||
+			!deserialiser.readBool(m_measureAtCheckpoint) ||
 			!deserialiser.readInt(m_prevId) ||
 			!deserialiser.readDouble(m_prevLongitude) ||
 			!deserialiser.readDouble(m_prevLatitude) ||
@@ -66,8 +66,8 @@ public:
     int     nextDeclination()   	const { return m_nextDeclination; }
     int     nextRadius()        	const { return m_nextRadius; }
     int     stayTime()          	const { return m_nextStayTime; }
-	double	timeInterval()			const { return m_nextTimeInterval; }
-	bool	measureAtCheckpoint()	const { return m_nextMeasureAtCheckpoint; }
+	double	timeInterval()			const { return m_timeInterval; }
+	bool	measureAtCheckpoint()	const { return m_measureAtCheckpoint; }
 
     int     prevId()            const { return m_prevId; }
     double  prevLongitude()     const { return m_prevLongitude; }
@@ -94,8 +94,8 @@ public:
 		serialiser.serialise(m_prevDeclination);
 		serialiser.serialise(m_prevRadius);
 
-		serialiser.serialise(m_nextTimeInterval);
-		serialiser.serialise(m_nextMeasureAtCheckpoint);
+		serialiser.serialise(m_timeInterval);
+		serialiser.serialise(m_measureAtCheckpoint);
 	}
 
 private:
@@ -106,8 +106,8 @@ private:
     int     m_nextDeclination;	// units : degrees
     int     m_nextRadius;		// units : meters
     int     m_nextStayTime;		// units : seconds
-	double	m_nextTimeInterval;	// units : seconds
-	bool	m_nextMeasureAtCheckpoint;	// units : true or false
+	double	m_timeInterval;	// units : seconds
+	bool	m_measureAtCheckpoint;	// units : true or false
 
     int     m_prevId;
     double  m_prevLongitude;	// units : North(+) or South(-) [0-90]
