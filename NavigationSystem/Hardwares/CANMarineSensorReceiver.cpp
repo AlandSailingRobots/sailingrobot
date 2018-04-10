050b7c01eccb179d2a57f497890c4516ae889558
Charlotta Solax <charlotta.solax@ha.ax>
2018-04-10 16:00:48 +0300

    Made changes in types from int to uint to make them compileible whit the rpi and again change the code to an if-else for how to read errormessage with new references

--- NavigationSystem/Hardwares/CANMarineSensorReceiver.cpp
+++ NavigationSystem/Hardwares/CANMarineSensorReceiver.cpp
@@ -6,24 +6,24 @@
 #include "SystemServices/Logger.h"
 
 
-const int ARD_MIN_PH = 0;
+const uint8_t ARD_MIN_PH = 0;
 const float MIN_PH = 0;
-const int ARD_MAX_PH = 255;
+const uint8_t ARD_MAX_PH = 255;
 const float MAX_PH = 14;
 
-const int ARD_MIN_CON = 0;
+const uint8_t ARD_MIN_CON = 0;
 const float MIN_CON = 5;
-const long int ARD_MAX_CON = 4294967295;
+const uint32_t ARD_MAX_CON = 4294967295;
 const float MAX_CON = 200000;
 
-const int ARD_MIN_TEMP = 0;
+const uint16_t ARD_MIN_TEMP = 0;
 const float MIN_TEMP = -5;
-const int ARD_MAX_TEMP = 65535;
+const uint16_t ARD_MAX_TEMP = 65535;
 const float MAX_TEMP = 40;
 
-const int ARD_MIN_ERROR = 0;
+const uint16_t ARD_MIN_ERROR = 0;
 const float MIN_ERROR = -5;
-const int ARD_MAX_ERROR = 65535;
+const uint16_t ARD_MAX_ERROR = 65535;
 const float MAX_ERROR = 40;
 
 #define SUCCESS 0
@@ -46,14 +46,14 @@ void CANDatalistener::processFrame (CanMsg& msg) {
 
     //provided that msg frameID is 711 nothing else
 
-    uint8_t rawErrorMessage = (msg.data[1] << 4 | msg.data[0]);
+    uint8_t rawErrorMessage = (msg.data[0]);
     
     Logger logError;
     logError.init("CANMarineSensorReceiver");
  
-    switch(rawErrorMessage) {
+    /*switch(rawErrorMessage) {
     case SUCCESS :{
-            uint8_t rawPh = (msg.data[1] << 4 | msg.data[0]);
+            uint8_t rawPh = (msg.data[1]);
             float   ph  = Utility::mapInterval (rawPh,ARD_MIN_PH,ARD_MAX_PH,MIN_PH,MAX_PH); //extract from CanMsg and convert
 
             uint32_t rawCon = (msg.data[5] << 24 | msg.data[4] << 16 | msg.data[3] << 8 | msg.data[2] );
@@ -80,12 +80,12 @@ void CANDatalistener::processFrame (CanMsg& msg) {
              
     default:            
         logError.warning("Was not able to read CanMsg ErrorMessage 0 to 3 from CANMarineSensorReceiver ");
-}
+}*/
 
 
 
-    /*if(static_cast<int>(ErrorMessage)==0){
-            uint16_t rawPh = (msg.data[1] << 4 | msg.data[0]);
+    if(static_cast<int>(ErrorMessage)==SUCCESS){
+            uint16_t rawPh = (msg.data[1]);
             float   ph  = Utility::mapInterval (rawPh,ARD_MIN_PH,ARD_MAX_PH,MIN_PH,MAX_PH); //extract from CanMsg and convert
 
             uint16_t rawCon = (msg.data[5] << 24 | msg.data[4] << 16 | msg.data[3] << 8 | msg.data[2] );
@@ -98,17 +98,8 @@ void CANDatalistener::processFrame (CanMsg& msg) {
             MessagePtr marineSensorDataMsg = std::make_unique<MarineSensorDataMsg>(temp, conductivety, ph, salinity);
             m_msgBus.sendMessage(std::move(marineSensorDataMsg));
     }
-    else if(static_cast<int>(ErrorMessage)==1){
-        logError.warning("Syntax Error in CANMarineSensorReceiver");
+    else {
+      
     }
-    else if(static_cast<int>(ErrorMessage)==2){
-          logError.warning("CANMarineSensorReceiver is not ready ");
-    }
-    else if(static_cast<int>(ErrorMessage)==3){
-         logError.warning("CANMarineSensorReceiver has no data to send");
-    }
-    else{
-        logError.warning("Was not able to read CanMsg ErrorMessage 0 to 3 from CANMarineSensorReceiver ");
-    }
-        */   
+          
 }
