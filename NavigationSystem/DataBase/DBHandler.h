85ec9ff237880e01b8c42e27ef1cd3adf730482e
Kåre Hampf <kare.hampf@ha.ax>
2018-04-24 11:04:30 +0300

    Empty string detection before stoi

--- NavigationSystem/DataBase/DBHandler.h
+++ NavigationSystem/DataBase/DBHandler.h
@@ -1,175 +1,197 @@
 #ifndef __DBHANDLER_H__
-#define __DBHANDLER_H__ //__DATACOLLECT_H__
+#define __DBHANDLER_H__  //__DATACOLLECT_H__
 
+#include <sqlite3.h>
 #include <iostream>
+#include <mutex>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <sqlite3.h>
-#include "SystemServices/Logger.h"
 #include "Messages/WindStateMsg.h"
-#include <mutex>
+#include "SystemServices/Logger.h"
 
 #include "Libs/json/src/json.hpp"
 using Json = nlohmann::json;
 
 struct LogItem {
-		double	m_rudderPosition; //dataLogs_actuator_feedback
-		double	m_wingsailPosition;
-		bool	m_radioControllerOn;
-		double	m_windVaneAngle;
-		double 	m_compassHeading; // dataLogs_compass
-		double 	m_compassPitch;
-		double 	m_compassRoll;
-		double 	m_distanceToWaypoint;//dataLogs_course_calculation
-		double 	m_bearingToWaypoint;
-		double 	m_courseToSteer;
-		bool 	m_tack;
-		bool 	m_goingStarboard;
-		double 	m_currentActuatorUnit;//dataLogs_current_sensors
-		double 	m_currentNavigationUnit;
-		double 	m_currentWindVaneAngle;
-		double 	m_currentWindVaneClutch;
-		double 	m_currentSailboatDrive;
-		bool	m_gpsHasFix; //dataLogs_gps
-		bool	m_gpsOnline;
-		double	m_gpsLat;
-		double	m_gpsLon;
-		double	m_gpsUnixTime;
-		double	m_gpsSpeed;
-		double	m_gpsCourse;
-		int		m_gpsSatellite;
-		bool 	m_routeStarted;
-		float   m_temperature;//dataLogs_marine_sensors
-		float   m_conductivity;
-		float   m_ph;
-    float   m_salinity;
-		double	m_vesselHeading;//dataLogs_vessel_state
-		double	m_vesselLat;
-		double	m_vesselLon;
-		double	m_vesselSpeed;
-		double	m_vesselCourse;
-		double	m_trueWindSpeed; //dataLogs_wind_state
-		double	m_trueWindDir;
-		double	m_apparentWindSpeed;
-		double	m_apparentWindDir;
-		float	m_windDir; //dataLogs_windsensor
-		float	m_windSpeed;
-		float 	m_windTemp;
-		std::string m_timestamp_str;
-	};
+    double m_rudderPosition;  // dataLogs_actuator_feedback
+    double m_wingsailPosition;
+    bool m_radioControllerOn;
+    double m_windVaneAngle;
+    double m_compassHeading;  // dataLogs_compass
+    double m_compassPitch;
+    double m_compassRoll;
+    double m_distanceToWaypoint;  // dataLogs_course_calculation
+    double m_bearingToWaypoint;
+    double m_courseToSteer;
+    bool m_tack;
+    bool m_goingStarboard;
+    double m_currentActuatorUnit;  // dataLogs_current_sensors
+    double m_currentNavigationUnit;
+    double m_currentWindVaneAngle;
+    double m_currentWindVaneClutch;
+    double m_currentSailboatDrive;
+    bool m_gpsHasFix;  // dataLogs_gps
+    bool m_gpsOnline;
+    double m_gpsLat;
+    double m_gpsLon;
+    double m_gpsUnixTime;
+    double m_gpsSpeed;
+    double m_gpsCourse;
+    int m_gpsSatellite;
+    bool m_routeStarted;
+    float m_temperature;  // dataLogs_marine_sensors
+    float m_conductivity;
+    float m_ph;
+    float m_salinity;
+    double m_vesselHeading;  // dataLogs_vessel_state
+    double m_vesselLat;
+    double m_vesselLon;
+    double m_vesselSpeed;
+    double m_vesselCourse;
+    double m_trueWindSpeed;  // dataLogs_wind_state
+    double m_trueWindDir;
+    double m_apparentWindSpeed;
+    double m_apparentWindDir;
+    float m_windDir;  // dataLogs_windsensor
+    float m_windSpeed;
+    float m_windTemp;
+    std::string m_timestamp_str;
+};
 
 class DBHandler {
+   private:
+    char* m_error;
+    int m_latestDataLogId;
+    std::string m_currentWaypointId = "";
+    std::string m_filePath;
+    static std::mutex m_databaseLock;
 
-private:
+    // execute INSERT query and add new row into table
+    bool queryTable(std::string sqlINSERT);
+    bool queryTable(std::string sqlINSERT, sqlite3* db);
 
-	char* m_error;
-	int m_latestDataLogId;
-	std::string m_currentWaypointId = "";
-	std::string m_filePath;
-	static std::mutex m_databaseLock;
+    // retrieve data from given table/tables, return value is a C 2D char array
+    // rows and columns also return values (through a reference) about rows and columns in the
+    // result set
+    std::vector<std::string> retrieveFromTable(std::string sqlSELECT, int& rows, int& columns);
+    std::vector<std::string> retrieveFromTable(std::string sqlSELECT,
+                                               int& rows,
+                                               int& columns,
+                                               sqlite3* db);
 
-	//execute INSERT query and add new row into table
-	bool queryTable(std::string sqlINSERT);
-	bool queryTable(std::string sqlINSERT, sqlite3* db);
+    // adds a table row into the json object as a array if array flag is true,
+    // otherwise it adds the table row as a json object
+    // id field is not obligatory, can be left empty
+    void getDataAsJson(std::string select,
+                       std::string table,
+                       std::string key,
+                       std::string id,
+                       Json& js,
+                       bool useArray);
 
-	//retrieve data from given table/tables, return value is a C 2D char array
-	//rows and columns also return values (through a reference) about rows and columns in the result set
-	std::vector<std::string> retrieveFromTable(std::string sqlSELECT, int &rows, int &columns);
-	std::vector<std::string> retrieveFromTable(std::string sqlSELECT, int &rows, int &columns,sqlite3* db);
+    // gets the id column from a given table
+    std::vector<std::string> getTableIds(std::string table);
 
-	//adds a table row into the json object as a array if array flag is true,
-	//otherwise it adds the table row as a json object
-	//id field is not obligatory, can be left empty
-	void getDataAsJson(std::string select, std::string table, std::string key, std::string id, Json& js, bool useArray);
+    // gets all datatable names related to "ending" string
+    // used to fetch all tables ending with _datalogs or _config
+    std::vector<std::string> getTableNames(std::string like);
 
-	//gets the id column from a given table
-	std::vector<std::string> getTableIds(std::string table);
+    // gets information(for instance: name/datatype) about all columns
+    std::vector<std::string> getColumnInfo(std::string info, std::string table);
 
-	// gets all datatable names related to "ending" string
-	//used to fetch all tables ending with _datalogs or _config
-	std::vector<std::string> getTableNames(std::string like);
+    // help function used in insertDataLog
+    int insertLog(std::string table, std::string values, sqlite3* db);
 
-	//gets information(for instance: name/datatype) about all columns
-	std::vector<std::string> getColumnInfo(std::string info, std::string table);
+    // own implementation of deprecated sqlite3_get_table()
+    int getTable(sqlite3* db,
+                 const std::string& sql,
+                 std::vector<std::string>& results,
+                 int& rows,
+                 int& columns);
 
-	//help function used in insertDataLog
-	int insertLog(std::string table, std::string values, sqlite3* db);
+    sqlite3* openDatabase();
 
-	// own implementation of deprecated sqlite3_get_table()
-	int getTable(sqlite3* db, const std::string &sql, std::vector<std::string> &results, int &rows, int &columns);
+    void closeDatabase(sqlite3* connection);
 
-	sqlite3* openDatabase();
+   public:
+    DBHandler(std::string filePath);
+    ~DBHandler(void);
 
-	void closeDatabase(sqlite3* connection);
+    bool initialise();
 
+    int getRows(std::string table);
 
-public:
+    void insertDataLogs(std::vector<LogItem>& logs);
 
-	DBHandler(std::string filePath);
-	~DBHandler(void);
+    void insertMessageLog(std::string gps_time, std::string type, std::string msg);
 
-	bool initialise();
+    // updates table with json string (data)
+    bool updateTableJson(std::string table, std::string data);
+    bool updateTableJsonObject(std::string table, Json data);
 
-	int getRows(std::string table);
+    // updates table using values given
+    bool updateTable(std::string table, std::string column, std::string value, std::string id);
 
-	void insertDataLogs(std::vector<LogItem>& logs);
+    void clearTable(std::string table);
 
-	void insertMessageLog(std::string gps_time, std::string type, std::string msg);
+    void updateConfigs(std::string configs);
+    bool updateWaypoints(std::string waypoints);
 
-	//updates table with json string (data)
-	bool updateTableJson(std::string table, std::string data);
-	bool updateTableJsonObject(std::string table, Json data);
+    // retrieve one value from a table as string
+    std::string retrieveCell(std::string table, std::string id, std::string column);
 
-	//updates table using values given
-	bool updateTable(std::string table, std::string column, std::string value, std::string id);
+    // retrieve one value from a table as integer
+    int retrieveCellAsInt(std::string table, std::string id, std::string column);
 
-	void clearTable(std::string table);
+    // retrieve one value from a table as double
+    double retrieveCellAsDouble(std::string table, std::string id, std::string column);
 
-	void updateConfigs(std::string configs);
-	bool updateWaypoints(std::string waypoints);
+    // returns all logs in database as json; supply onlyLatest to get only the ones with the highest
+    // id
+    std::string getLogs(bool onlyLatest);
 
-    //retrieve one value from a table as string
-	std::string retrieveCell(std::string table, std::string id, std::string column);
+    void forceUnlock() { m_databaseLock.unlock(); }
 
-    //retrieve one value from a table as integer
-	int retrieveCellAsInt(std::string table, std::string id, std::string column);
+    void clearLogs();
 
-	//retrieve one value from a table as double
-	double retrieveCellAsDouble(std::string table, std::string id, std::string column);
+    // get id from table returns either max or min id from table.
+    // max = false -> min id
+    // max = true -> max id
+    std::string getIdFromTable(std::string table, bool max);
+    std::string getIdFromTable(std::string table, bool max, sqlite3* db);
 
-	// returns all logs in database as json; supply onlyLatest to get only the ones with the highest id
-	std::string getLogs(bool onlyLatest);
+    void deleteRow(std::string table, std::string id);
 
-	void forceUnlock() { m_databaseLock.unlock(); }
+    bool getWaypointValues(int& nextId,
+                           double& nextLongitude,
+                           double& nextLatitude,
+                           int& nextDeclination,
+                           int& nextRadius,
+                           int& nextStayTime,
+                           bool& isCheckpoint,
+                           int& prevId,
+                           double& prevLongitude,
+                           double& prevLatitude,
+                           int& prevDeclination,
+                           int& prevRadius,
+                           bool& foundPrev);
 
-	void clearLogs();
+    bool insert(std::string table, std::string fields, std::string values);
 
-	//get id from table returns either max or min id from table.
-	//max = false -> min id
-	//max = true -> max id
-	std::string getIdFromTable(std::string table, bool max);
-	std::string getIdFromTable(std::string table, bool max,sqlite3* db);
+    // inserts area scanning measurements into db
+    // TODO - remove here as well yeyeye
+    // void insertScan(std::string waypoint_id, PositionModel position, float temperature,
+    // std::string timestamp);
 
-	void deleteRow(std::string table, std::string id);
+    bool changeOneValue(std::string table,
+                        std::string id,
+                        std::string newValue,
+                        std::string colName);
 
-	bool getWaypointValues(int& nextId, double& nextLongitude, double& nextLatitude, int& nextDeclination,
-						int& nextRadius, int& nextStayTime,
-						bool& isCheckpoint, int& prevId, double& prevLongitude,
-						double& prevLatitude, int& prevDeclination, int& prevRadius, bool& foundPrev);
+    std::string getWaypoints();
 
-	bool insert(std::string table, std::string fields, std::string values);
-
-	// inserts area scanning measurements into db
-	//TODO - remove here as well yeyeye
-	//void insertScan(std::string waypoint_id, PositionModel position, float temperature, std::string timestamp);
-
-	bool changeOneValue(std::string table, std::string id, std::string newValue, std::string colName);
-
-	std::string getWaypoints();
-
-	std::string getConfigs();
-
+    std::string getConfigs();
 };
 
 #endif
