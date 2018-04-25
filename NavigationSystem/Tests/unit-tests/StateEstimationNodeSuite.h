0f55fa077d6c82f138001cdb7cb351d17359f113
Emma Brandt <emma.brandt@ha.ax>
2018-04-25 10:50:02 +0300

    state estimation unit tests updated and formatted

--- NavigationSystem/Tests/unit-tests/StateEstimationNodeSuite.h
+++ NavigationSystem/Tests/unit-tests/StateEstimationNodeSuite.h
@@ -1,40 +1,36 @@
 /****************************************************************************************
-*
-* File:
-* 		StateEstimationNodeSuite.h
-*
-* Purpose:
-*		A set of unit tests for checking if the StateEstimationNode works as intended
-*
-* Developer Notes:
-*
-*
-***************************************************************************************/
+ *
+ * File:
+ * 		StateEstimationNodeSuite.h
+ *
+ * Purpose:
+ *		A set of unit tests for checking if the StateEstimationNode works as intended
+ *
+ * Developer Notes:
+ *
+ *
+ ***************************************************************************************/
 
 #pragma once
 
-#include "WorldState/StateEstimationNode.h"
-#include "../cxxtest/cxxtest/TestSuite.h"
-#include "Messages/StateMessage.h"
 #include "../../MessageBus/MessageBus.h"
-#include "TestMocks/MessageLogger.h"
-#include "Messages/CompassDataMsg.h"
-#include "Messages/GPSDataMsg.h"
-#include "SystemServices/Timer.h"
+#include "../cxxtest/cxxtest/TestSuite.h"
 #include "DataBase/DBHandler.h"
 #include "Math/Utility.h"
 #include "MessageBusTestHelper.h"
+#include "Messages/CompassDataMsg.h"
+#include "Messages/GPSDataMsg.h"
+#include "Messages/StateMessage.h"
+#include "SystemServices/Timer.h"
+#include "TestMocks/MessageLogger.h"
+#include "WorldState/StateEstimationNode.h"
 
-// For std::this_thread
 #include <chrono>
 
-#define STATE_ESTIMATIONODE_TEST_COUNT   8
-
+#define STATE_ESTIMATIONODE_TEST_COUNT 8
 
 class StateEstimationNodeSuite : public CxxTest::TestSuite {
-
-public:
-
+   public:
     std::unique_ptr<StateEstimationNode> sEstimationNode;
 
     std::unique_ptr<DBHandler> dbhandler;
@@ -44,53 +40,44 @@ public:
     MessageBus messageBus;
     std::unique_ptr<MessageBusTestHelper> messageBusHelper;
 
-
     int testCount = 0;
 
-
-  // ----------------
-  // Setup the objects to test
-  // ----------------
-  void setUp()
-  {
-     // setup them up once in this test, delete them when the program closes
-    if(sEstimationNode == 0){
-        mockNode.reset(new MockNode(messageBus, nodeRegistered));
-        Logger::DisableLogging();
-        dbhandler.reset(new DBHandler("../asr.db"));
-        sEstimationNode.reset(new StateEstimationNode(messageBus,*dbhandler));
-        sEstimationNode->start();
-        std::this_thread::sleep_for(std::chrono::milliseconds(2600));
-        messageBusHelper.reset(new MessageBusTestHelper(messageBus));
-     }
-    testCount++;
-  }
-
-  // ----------------
-  // End of test when all test have been successfull
-  // ----------------
-  void tearDown()
-  {
-    if(testCount == STATE_ESTIMATIONODE_TEST_COUNT)
-    {
-        sEstimationNode -> stop();
-        messageBusHelper.reset();
+    // ----------------
+    // Setup the objects to test
+    // ----------------
+    void setUp() {
+        // setup them up once in this test, delete them when the program closes
+        if (sEstimationNode == 0) {
+            mockNode.reset(new MockNode(messageBus, nodeRegistered));
+            Logger::DisableLogging();
+            dbhandler.reset(new DBHandler("../asr.db"));
+            sEstimationNode.reset(new StateEstimationNode(messageBus, *dbhandler));
+            sEstimationNode->start();
+            std::this_thread::sleep_for(std::chrono::milliseconds(2600));
+            messageBusHelper.reset(new MessageBusTestHelper(messageBus));
+        }
+        testCount++;
     }
-  }
+
+    // ----------------
+    // End of test when all test have been successfull
+    // ----------------
+    void tearDown() {
+        if (testCount == STATE_ESTIMATIONODE_TEST_COUNT) {
+            sEstimationNode->stop();
+            messageBusHelper.reset();
+        }
+    }
 
     // ----------------
     // Test Initialisation of the object
     // ----------------
-    void test_StateEstimationNodeInit()
-    {
-        TS_ASSERT(nodeRegistered);
-    }
+    void test_StateEstimationNodeInit() { TS_ASSERT(nodeRegistered); }
 
     // ----------------
     // Test for the absence of a returned message by a offline GPS
     // ----------------
-    void test_StateEstimationNodeGPSNotOnline()
-    {
+    void test_StateEstimationNodeGPSNotOnline() {
         TS_ASSERT(sEstimationNode->init());
         TS_ASSERT(!mockNode->m_MessageReceived);
     }
@@ -98,8 +85,7 @@ public:
     // ----------------
     // Test to see if a message concerning the node will be listened
     // ----------------
-    void test_StateMessageListener()
-    {
+    void test_StateMessageListener() {
         double latitude = 60.09726;
         double longitude = 19.93481;
         double unixTime = 1;
@@ -108,82 +94,83 @@ public:
         int satCount = 2;
         GPSMode mode = GPSMode::LatLonOk;
 
-        MessagePtr gpsData =  std::make_unique<GPSDataMsg>(false,true,latitude,longitude,unixTime,speed,heading,satCount,
-          mode);
+        MessagePtr gpsData = std::make_unique<GPSDataMsg>(false, true, latitude, longitude,
+                                                          unixTime, speed, heading, satCount, mode);
         messageBus.sendMessage(std::move(gpsData));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
         TS_ASSERT(mockNode->m_MessageReceived);
     }
 
-  // ----------------
-  // Test to see if ,after the GPS messsage, the heading is not changed
-  // ----------------
-  void test_StateEstimationStateMsgHeading(){
-      double latitude = 60.09726;
-      double longitude = 19.93481;
-      double unixTime = 1;
-      double speed = 1;
-      double headingGPS = 10;
-      int satCount = 2;
-      GPSMode mode = GPSMode::LatLonOk;
+    // ----------------
+    // Test to see if ,after the GPS messsage, the heading is not changed
+    // ----------------
+    void test_StateEstimationStateMsgHeading() {
+        double latitude = 60.09726;
+        double longitude = 19.93481;
+        double unixTime = 1;
+        double speed = 1;
+        double headingGPS = 10;
+        int satCount = 2;
+        GPSMode mode = GPSMode::LatLonOk;
 
-        MessagePtr gpsData =  std::make_unique<GPSDataMsg>(false,true,latitude,longitude,unixTime,speed,headingGPS,satCount,
-        mode);
+        MessagePtr gpsData = std::make_unique<GPSDataMsg>(
+            false, true, latitude, longitude, unixTime, speed, headingGPS, satCount, mode);
         messageBus.sendMessage(std::move(gpsData));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-        int nextDeclination = 10;
-        // TODO : Check the constructor because the variables seems not appropriate.
-        MessagePtr wayPointMsgData = std::make_unique<WaypointDataMsg>(2, 19.81, 60.2, nextDeclination, 6, 15, true, 1, 19.82, 60.1, 6, 15);
-        messageBus.sendMessage(std::move(wayPointMsgData));
+        int nextDeclination = 6;
+        MessagePtr wayPointDataMsg = std::make_unique<WaypointDataMsg>(
+            2, 19.922311, 60.105700, nextDeclination, 12, 3, false, 1, longitude, latitude, 6, 20);
+
+        messageBus.sendMessage(std::move(wayPointDataMsg));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        TS_SKIP("Skipping failing test, needs update");
-       // TS_ASSERT_DELTA(mockNode->m_StateMsgHeading, 0, 1e-7); // Check if Heading of the state message is null because we must wait a data from the compass message
+
+        TS_ASSERT_DELTA(mockNode->m_StateMsgHeading, 0, 1e-7);  // Check if Heading of the state
+        // message is null because we must wait for data from the compass message
 
         int heading = 100;
-        MessagePtr compassData =  std::make_unique<CompassDataMsg>(heading, 80, 60);
+        MessagePtr compassData = std::make_unique<CompassDataMsg>(heading, 80, 60);
         messageBus.sendMessage(std::move(compassData));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-        //Check if there is the same result by the processing next to the Compass data has been received
+        // Check if there is the same result by the processing next to the Compass data has been
+        // received
         float vesselHeading = Utility::addDeclinationToHeading(heading, nextDeclination);
         float stateEstimationNodeVesselHeading = mockNode->m_StateMsgHeading;
         TS_ASSERT_EQUALS(stateEstimationNodeVesselHeading, vesselHeading);
     }
 
-      // ----------------
-      // Test to see if the message, sent by the node, is with the values
-      // ----------------
-      void test_StateEstimationStateMessageGPSData(){
+    // ----------------
+    // Test to see if the message, sent by the node, is with the values
+    // ----------------
+    void test_StateEstimationStateMessageGPSData() {
         double latitude = 60.09726;
         double longitude = 19.93481;
         double unixTime = 1;
-        double speed = 1;
-        double heading = 10;
+        double speed = -1;
+        double heading = 190;
         int satCount = 2;
         GPSMode mode = GPSMode::LatLonOk;
 
-        MessagePtr gpsData =  std::make_unique<GPSDataMsg>(false,true,latitude,longitude,unixTime,speed,heading,satCount,
-        mode);
+        MessagePtr gpsData = std::make_unique<GPSDataMsg>(false, true, latitude, longitude,
+                                                          unixTime, speed, heading, satCount, mode);
         messageBus.sendMessage(std::move(gpsData));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
         TS_ASSERT_EQUALS(mockNode->m_StateMsgLat, latitude);
         TS_ASSERT_EQUALS(mockNode->m_StateMsgLon, longitude);
-        TS_SKIP("Skipping failing test, needs update");
-        //TS_ASSERT_EQUALS(mockNode->m_StateMsgSpeed, speed);
-        TS_SKIP("Skipping failing test, needs update");
-        //TS_ASSERT_EQUALS(mockNode->m_StateMsgCourse, heading);
+        TS_ASSERT_EQUALS(mockNode->m_StateMsgSpeed, speed);
+        TS_ASSERT_EQUALS(mockNode->m_StateMsgCourse, heading);
     }
 
     // ----------------
     // Test to see if the message, sent by the node, is with the values
     // ----------------
-    void test_StateEstStateMsgSpeedAndDeclZero()
-    {
+    void test_StateEstStateMsgSpeedAndDeclZero() {
         int nextDeclination = 0;
-        MessagePtr wayPointMsgData = std::make_unique<WaypointDataMsg>(2, 19.81, 60.2, nextDeclination, 6, 15, true, 1, 19.82, 60.1, 6, 15);
+        MessagePtr wayPointMsgData = std::make_unique<WaypointDataMsg>(
+            2, 19.81, 60.2, nextDeclination, 6, 15, true, 1, 19.82, 60.1, 6, 15);
         messageBus.sendMessage(std::move(wayPointMsgData));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
@@ -199,16 +186,15 @@ public:
         double headingGPS = 10;
         int satCount = 2;
         GPSMode mode = GPSMode::LatLonOk;
-        MessagePtr gpsMsgData = std::make_unique<GPSDataMsg>(false,true,latitude,longitude,unixTime,speed,headingGPS,satCount,
-        mode);
+        MessagePtr gpsMsgData = std::make_unique<GPSDataMsg>(
+            false, true, latitude, longitude, unixTime, speed, headingGPS, satCount, mode);
         messageBus.sendMessage(std::move(gpsMsgData));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
         TS_ASSERT_DELTA(mockNode->m_StateMsgCourse, 0, 1e-7);
     }
 
-    void test_StateEstStateMsgSpeedAndDeclOverZero()
-    {
+    void test_StateEstStateMsgSpeedAndDeclOverZero() {
         double latitude = 60.09726;
         double longitude = 19.93481;
         double unixTime = 1;
@@ -217,13 +203,14 @@ public:
         int satCount = 2;
         GPSMode mode = GPSMode::LatLonOk;
 
-        MessagePtr gpsMsgData = std::make_unique<GPSDataMsg>(false,true,latitude,longitude,unixTime,speed,headingGPS,satCount,
-        mode);
+        MessagePtr gpsMsgData = std::make_unique<GPSDataMsg>(
+            false, true, latitude, longitude, unixTime, speed, headingGPS, satCount, mode);
         messageBus.sendMessage(std::move(gpsMsgData));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
         int nextDeclination = 10;
-        MessagePtr wayPointMsgData = std::make_unique<WaypointDataMsg>(2, 19.81, 60.2, nextDeclination, 6, 15, true, 1, 19.82, 60.1, 6, 15);
+        MessagePtr wayPointMsgData = std::make_unique<WaypointDataMsg>(
+            2, 19.81, 60.2, nextDeclination, 6, 15, true, 1, 19.82, 60.1, 6, 15);
         messageBus.sendMessage(std::move(wayPointMsgData));
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
@@ -237,82 +224,81 @@ public:
         TS_ASSERT(mockNode->m_StateMsgCourse > headingGPS);
     }
 
-    void test_StateEstStateMsgSpeedLessThanZero()
-    {
+    void test_StateEstStateMsgSpeedLessThanZero() {
         double latitude = 60.09726;
         double longitude = 19.93481;
         double unixTime = 1;
         double speed = -1;
-        double headingGPS = 10.00;
+        double headingGPS = 160.00;
         int satCount = 2;
         GPSMode mode = GPSMode::LatLonOk;
 
-        MessagePtr gpsMsgData = std::make_unique<GPSDataMsg>(false,true,latitude,longitude,unixTime,speed,headingGPS,satCount,
-        mode);
+        MessagePtr gpsMsgData = std::make_unique<GPSDataMsg>(
+            false, true, latitude, longitude, unixTime, speed, headingGPS, satCount, mode);
         messageBus.sendMessage(std::move(gpsMsgData));
 
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
         int nextDeclination = 10;
-        MessagePtr wayPointMsgData = std::make_unique<WaypointDataMsg>(2, 19.81, 60.2, nextDeclination, 6, 15, true, 1, 19.82, 60.1, 6, 15);
+        MessagePtr wayPointMsgData = std::make_unique<WaypointDataMsg>(
+            2, 19.81, 60.2, nextDeclination, 6, 15, true, 1, 19.82, 60.1, 6, 15);
         messageBus.sendMessage(std::move(wayPointMsgData));
 
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
         int heading = 100;
         MessagePtr compassMsgData = std::make_unique<CompassDataMsg>(heading, 80, 60);
-        MessagePtr compassData =  std::make_unique<CompassDataMsg>(heading, 80, 60);
+        MessagePtr compassData = std::make_unique<CompassDataMsg>(heading, 80, 60);
         messageBus.sendMessage(std::move(compassData));
 
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
         float stateEstimationNodeVesselHeading = mockNode->m_StateMsgHeading;
         TS_ASSERT(stateEstimationNodeVesselHeading != 0);
-        TS_SKIP("Skipping erroneous course test, needs checking");
-//        TS_ASSERT_DELTA(mockNode->m_StateMsgCourse, headingGPS, 1e-7);
+        TS_ASSERT_DELTA(mockNode->m_StateMsgCourse, headingGPS, 1e-7);
     }
 
     // ----------------
     // Test for update frequency
     // ----------------
-    void test_StateEstimationUpdateFrequency(){
+    void test_StateEstimationUpdateFrequency() {
         TS_SKIP("Failing test (hanging) skipped");
-/*
-        Timer timer;
+        /*
+                Timer timer;
 
-        dbhandler->changeOneValue("config_vessel_state","1","0.7","loop_time");
-        MessagePtr serverConfig = std::make_unique<ServerConfigsReceivedMsg>();
-        messageBus.sendMessage(std::move(serverConfig));
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        TS_ASSERT(mockNode->m_MessageReceived);
+                dbhandler->changeOneValue("config_vessel_state","1","0.7","loop_time");
+                MessagePtr serverConfig = std::make_unique<ServerConfigsReceivedMsg>();
+                messageBus.sendMessage(std::move(serverConfig));
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                TS_ASSERT(mockNode->m_MessageReceived);
 
-        mockNode->m_MessageReceived = false;
-        while(not mockNode->m_MessageReceived);   //Hanging here
-        timer.start();
-        mockNode->m_MessageReceived = false;
-        while(not mockNode->m_MessageReceived);
-        timer.stop();
+                mockNode->m_MessageReceived = false;
+                while(not mockNode->m_MessageReceived);   //Hanging here
+                timer.start();
+                mockNode->m_MessageReceived = false;
+                while(not mockNode->m_MessageReceived);
+                timer.stop();
 
-        TS_ASSERT_DELTA(timer.timePassed(), 0.70, 1e-2);
-*/
-        // double heading = 10;
-        // double speed = 1;
-        // MaxRudAng = 20.0;
-        // double desiredcourse = 15;
-        //
-        // MessagePtr stateData = std::make_unique<StateMessage>(heading,60.09726,19.93481,speed,0);
-        // msgBus().sendMessage(std::move(stateData));
-        // std::this_thread::sleep_for(std::chrono::milliseconds(700));
-        //
-        // MessagePtr desiredCourseData = std::make_unique<DesiredCourseMsg>(desiredcourse);
-        // msgBus().sendMessage(std::move(desiredCourseData));
-        // std::this_thread::sleep_for(std::chrono::milliseconds(700));
-        //
-        // double diffHeading = Utility::limitAngleRange(heading)-Utility::limitAngleRange(desiredcourse);
-        // int rudderAngle = Utility::sgn(speed)*sin(Utility::degreeToRadian(diffHeading))*MaxRudAng;
-        // std::this_thread::sleep_for(std::chrono::milliseconds(700));
-        // double courseRegulatorRudderAngle = mockNode->m_rudderPosition;
-        // TS_ASSERT_DELTA(courseRegulatorRudderAngle, rudderAngle, 1e-7);
-        dbhandler->changeOneValue("config_vessel_state","1","0.5","loop_time");
+                TS_ASSERT_DELTA(timer.timePassed(), 0.70, 1e-2);
+
+         double heading = 10;
+         double speed = 1;
+         MaxRudAng = 20.0;
+         double desiredcourse = 15;
+
+         MessagePtr stateData = std::make_unique<StateMessage>(heading,60.09726,19.93481,speed,0);
+         msgBus().sendMessage(std::move(stateData));
+         std::this_thread::sleep_for(std::chrono::milliseconds(700));
+
+         MessagePtr desiredCourseData = std::make_unique<DesiredCourseMsg>(desiredcourse);
+         msgBus().sendMessage(std::move(desiredCourseData));
+         std::this_thread::sleep_for(std::chrono::milliseconds(700));
+
+         double diffHeading =
+         Utility::limitAngleRange(heading)-Utility::limitAngleRange(desiredcourse); int
+         rudderAngle = Utility::sgn(speed)*sin(Utility::degreeToRadian(diffHeading))*MaxRudAng;
+        std::this_thread::sleep_for(std::chrono::milliseconds(700));
+         double courseRegulatorRudderAngle = mockNode->m_rudderPosition;
+         TS_ASSERT_DELTA(courseRegulatorRudderAngle, rudderAngle, 1e-7);
+        dbhandler->changeOneValue("config_vessel_state", "1", "0.5", "loop_time"); */
     }
-
 };
