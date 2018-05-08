d0f3f007737ab5b31e0516d8a1bcccd72d9351db
Dag Karlsson <dag.karlsson@ha.ax>
2018-05-08 08:50:27 +0300

    Cleaned up MarineDataIntegrationTest

--- NavigationSystem/Tests/IntegrationTests/MarineDataIntegrationTest.cpp
+++ NavigationSystem/Tests/IntegrationTests/MarineDataIntegrationTest.cpp
@@ -3,11 +3,6 @@
 #include <thread>
 #include <sstream>
 
-
-#include "DataBase/DBHandler.h"
-#include "DataBase/DBLoggerNode.h"
-#include "HTTPSync/HTTPSyncNode.h"
-
 #include "Hardwares/CAN_Services/CANService.h"
 #include "Hardwares/CAN_Services/N2kMsg.h"
 #include "Hardwares/CAN_Services/CanBusCommon/canbus_defs.h"
@@ -134,8 +129,6 @@ int main(int argc, char *argv[])
 		db_path = std::string(argv[1]);
 	}
 
-	// Declare DBHandler and MessageBus
-	DBHandler dbHandler(db_path);
 	MessageBus messageBus;
 
 	// Initialise logger
@@ -150,53 +143,24 @@ int main(int argc, char *argv[])
 		Logger::error("Logger init\t\t[FAILED]");
 	}
 
-	// Initialise DBHandler
-	if(dbHandler.initialise())
-	{
-		Logger::info("Database Handler init\t\t[OK]");
-	}
-	else
-	{
-		Logger::error("Database Handler init\t\t[FAILED]");
-		Logger::shutdown();
-		exit(1);
-	}
-
-
 	// Declare nodes
 	//-------------------------------------------------------------------------------
 
-	int dbLoggerQueueSize = 5; 			// how many messages to log to the databse at a time
-	DBLoggerNode dbLoggerNode(messageBus, dbHandler, dbLoggerQueueSize);
-	HTTPSyncNode httpsync(messageBus, &dbHandler);
-
-
-
 	CANService canService;
 
-	GPSDNode gpsd(messageBus, dbHandler);
     MarineSensorReciever canMarineSensorReciver(messageBus, canService);
 	CANMarineSensorTransmissionNode canMarineSensorTransmissionNode(messageBus, canService);
 
 
 	// Initialise nodes
 	//-------------------------------------------------------------------------------
-
-	initialiseNode(httpsync, "Httpsync", NodeImportance::NOT_CRITICAL); // This node is not critical during the developement phase.
-	initialiseNode(dbLoggerNode, "DBLogger", NodeImportance::CRITICAL);
-
-    initialiseNode(gpsd, "GPSD", NodeImportance::CRITICAL);
     initialiseNode(canMarineSensorTransmissionNode, "Marine Sensors", NodeImportance::NOT_CRITICAL);
 
 
 	// Start active nodes
 	//-------------------------------------------------------------------------------
 
-	httpsync.start();
-	dbLoggerNode.start();
-
     auto future = canService.start();
-    gpsd.start();
 
     /*
      * Send a data request message.
