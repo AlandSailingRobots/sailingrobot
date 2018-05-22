48731a291107d684d9fa9cc788e705ecc0198741
Kåre Hampf <kare.hampf@ha.ax>
2018-05-22 11:01:37 +0300

    Editing

--- codebase.sh
+++ codebase.sh
@@ -2,14 +2,25 @@
 set -e
 
 # check $0 in pwd
+if [ ! -e "$(pwd)/$(basename "$0")" ]; then
+    printf 'Spooky, I do not exist in %s ...\n' "$(pwd)"
+    exit 1
+fi
 case "$1" in
     init)
         ;;
     update)
-        git pull
-        git submodule update
+        git pull &&
+            git submodule sync --recursive &&
+            git submodule update --init --recursive &&
+            printf '%s: Done\n' "$0"
+                    ;;
+
+    status)
+        git status
+        git submodule foreach "git status"
         ;;
     *)
-        printf 'Usage: %s update\n' "$0"
+        printf 'Usage: %s update|status\n' "$0"
         ;;
 esac
