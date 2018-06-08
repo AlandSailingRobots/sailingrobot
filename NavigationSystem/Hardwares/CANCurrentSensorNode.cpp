1b2467006d9cda589356bca2c89250b1e54caffa
Supercelll <yacine.benhnini@ensta-bretagne.org>
2018-06-08 13:13:16 +0300

    multiple sensor in logger working,still no push to database

--- NavigationSystem/Hardwares/CANCurrentSensorNode.cpp
+++ NavigationSystem/Hardwares/CANCurrentSensorNode.cpp
@@ -20,7 +20,7 @@
 const float DATA_OUT_OF_RANGE = -2000; // as uint16_t, cannot use -2000.
 
 CANCurrentSensorNode::CANCurrentSensorNode(MessageBus& messageBus, DBHandler& dbhandler, CANService& canService) :
-ActiveNode(NodeID::CANCurrentSensor, messageBus), CANFrameReceiver(canService, MSG_ID_CURRENT_SENSOR_DATA), m_LoopTime (0.5), m_db(dbhandler)
+ActiveNode(NodeID::CANCurrentSensor, messageBus), CANFrameReceiver(canService, {MSG_ID_CURRENT_SENSOR_DATA,MSG_ID_CURRENT_SENSOR_DATA_POWER_UNIT}), m_LoopTime (0.5), m_db(dbhandler)
 {
         m_current = DATA_OUT_OF_RANGE;
         m_voltage = DATA_OUT_OF_RANGE;
@@ -51,7 +51,7 @@ void CANCurrentSensorNode::processMessage (const Message* message){
 }
 
 void CANCurrentSensorNode::processFrame (CanMsg& msg) {
-    Logger::info("Received current sensor readings from CanBus");
+    //Logger::info("Received current sensor readings from CanBus");
     Float16Compressor fltCompressor;
 	CanMessageHandler messageHandler(msg);
 	uint16_t comp_current, comp_voltage;
@@ -67,11 +67,12 @@ void CANCurrentSensorNode::processFrame (CanMsg& msg) {
         m_current = fltCompressor.decompress(comp_current);
         m_voltage = fltCompressor.decompress(comp_voltage);
         
-        m_element = SAILDRIVE;        //TO TRY
-       // m_element = (SensedElement)((uint8_t)msg.id - (uint8_t)720);        //TO TRY
+        //m_element = SAILDRIVE;        //TO TRY
+        m_element = (SensedElement)((uint8_t)msg.id - (uint8_t)720);        //TO TRY
         MessagePtr currentSensorDataMsg = std::make_unique<CurrentSensorDataMsg>(static_cast<float>(m_current),
      		           static_cast<float>(m_voltage), static_cast<SensedElement>(m_element));
 //        m_msgBus.sendMessage(std::move(currentSensorDataMsg));
+        Logger::info("Current sensor data: Current: %lf , Voltage: %lf , Sensor: %d \n",m_current,m_voltage,m_element);
     }
 }
 
@@ -91,13 +92,13 @@ void CANCurrentSensorNode::CANCurrentSensorNodeThreadFunc(ActiveNode* nodePtr) {
 
 		node->m_lock.lock();
                 // Don´t forget modifs here
-		if( not (node->m_voltage == DATA_OUT_OF_RANGE &&  node->m_current == DATA_OUT_OF_RANGE))
+//		if( not (node->m_voltage == DATA_OUT_OF_RANGE &&  node->m_current == DATA_OUT_OF_RANGE))
 				// Skipped for now  && node->m_element == "undefined"))
-		{
+//		{
 			MessagePtr currentSensorData = std::make_unique<CurrentSensorDataMsg>( node->m_current, 
 				node->m_voltage, node->m_element);
 			node->m_MsgBus.sendMessage(std::move(currentSensorData));
-		}
+//		}
 		node->m_lock.unlock();
 
 		timer.sleepUntil(node->m_LoopTime);
