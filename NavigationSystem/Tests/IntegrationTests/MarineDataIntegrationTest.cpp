5d882538fe71f7b6c84a574e7ead1485c0bbf9a6
Supercelll <yacine.benhnini@ensta-bretagne.org>
2018-07-05 16:14:47 +0300

    Marine sensor integration test with new functions

--- NavigationSystem/Tests/IntegrationTests/MarineDataIntegrationTest.cpp
+++ NavigationSystem/Tests/IntegrationTests/MarineDataIntegrationTest.cpp
@@ -32,22 +32,20 @@ public:
     }
 
     void processFrame (CanMsg& msg) {
-        Logger::info("Recieved marine sensor readings from CanBus");
+        Logger::info("Received marine sensor readings from CanBus");
 
         CanMessageHandler handler(msg);
 
         if(handler.getMessageId() == MSG_ID_MARINE_SENSOR_DATA) {
-            double ph;
-			double conductivety;
-			double temp;
-            handler.getMappedData(&ph, SENSOR_PH_DATASIZE,
-                                              SENSOR_PH_INTERVAL_MIN, SENSOR_PH_INTERVAL_MAX);
+            float ph, conductivety, temperature;
+            handler.getMappedData(&ph, SENSOR_PH_START, SENSOR_PH_DATASIZE,
+                                       SENSOR_PH_IN_BYTE, SENSOR_PH_INTERVAL_MIN, SENSOR_PH_INTERVAL_MAX);
 
-            handler.getMappedData(&conductivety, SENSOR_CONDUCTIVETY_DATASIZE,
-                                                        SENSOR_CONDUCTIVETY_INTERVAL_MIN, SENSOR_CONDUCTIVETY_INTERVAL_MAX);
+            handler.getMappedData(&conductivety,SENSOR_CONDUCTIVETY_START, SENSOR_CONDUCTIVETY_DATASIZE,
+                                         SENSOR_CONDUCTIVETY_IN_BYTE, SENSOR_CONDUCTIVETY_INTERVAL_MIN, SENSOR_CONDUCTIVETY_INTERVAL_MAX);
 
-            handler.getMappedData(&temp, SENSOR_TEMPERATURE_DATASIZE,
-                                                SENSOR_TEMPERATURE_INTERVAL_MIN, SENSOR_TEMPERATURE_INTERVAL_MAX);
+            handler.getMappedData(&temp, SENSOR_TEMPERATURE_START, SENSOR_TEMPERATURE_DATASIZE,
+                                         SENSOR_TEMPERATURE_IN_BYTE, SENSOR_TEMPERATURE_INTERVAL_MIN, SENSOR_TEMPERATURE_INTERVAL_MAX);
             float salinity = Utility::calculateSalinity (temp, conductivety);
 
             MessagePtr marineSensorDataMsg = std::make_unique<MarineSensorDataMsg>(static_cast<float>(temp), static_cast<float>(conductivety), static_cast<float>(ph), salinity);
