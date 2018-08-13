ef01dee36438f1f9ad78a5c67a597349258ce7fd
François Morlet <francois.morlet@ha.ax>
2018-08-13 14:22:06 +0300

    Added compass measurement rate setting method
    Added HMC6343Node::setMeasurementRate
    Set to 10Hz on node init, if loop time is under 0.2s

--- NavigationSystem/Hardwares/HMC6343Node.cpp
+++ NavigationSystem/Hardwares/HMC6343Node.cpp
@@ -92,6 +92,9 @@ bool HMC6343Node::init()
 		Logger::error("%s Failed to obtain I2C file descriptor", __PRETTY_FUNCTION__);
 	}
 
+	if (m_LoopTime < 0.2)
+		setMeasurementRate(COM_MEASUREMENT_RATE_10HZ);
+
 	return m_Initialised;
 }
 
@@ -189,6 +192,22 @@ bool HMC6343Node::setOrientation(CompassOrientation orientation)
 	else
 	{
 		Logger::error("%s Cannot set compass orientation as the node was not correctly initialised!", __PRETTY_FUNCTION__);
+		return false;
+	}
+}
+
+bool HMC6343Node::setMeasurementRate(CompassMeasurementRate rate)
+{
+	if(m_Initialised)
+	{
+		m_I2C.beginTransmission();
+		m_I2C.writeReg(OM2_ADDRESS, (uint8_t)rate);
+		m_I2C.endTransmission();
+		return true;
+	}
+	else
+	{
+		Logger::error("%s Cannot set compass measurement rate as the node was not correctly initialised!", __PRETTY_FUNCTION__);
 		return false;
 	}
 }
--- NavigationSystem/Hardwares/HMC6343Node.h
+++ NavigationSystem/Hardwares/HMC6343Node.h
@@ -31,6 +31,13 @@ enum class CompassOrientation {
     COM_ORIENT_FLATFRONT = 0x74
 };
 
+enum class CompassMeasurementRate {
+    COM_MEASUREMENT_RATE_1HZ  = 0x00,
+    COM_MEASUREMENT_RATE_5HZ  = 0x02, // hardware default
+    COM_MEASUREMENT_RATE_10HZ = 0x03,
+    COM_MEASUREMENT_RATE_NA   = 0x04
+};
+
 class HMC6343Node : public ActiveNode {
    public:
     HMC6343Node(MessageBus& msgBus, DBHandler& dbhandler);
@@ -59,6 +66,11 @@ class HMC6343Node : public ActiveNode {
     /// Sets the compass's orientation.
     ///----------------------------------------------------------------------------------
     bool setOrientation(CompassOrientation orientation);
+
+    ///----------------------------------------------------------------------------------
+    /// Sets the compass's measurement rate.
+    ///----------------------------------------------------------------------------------
+    bool setMeasurementRate(CompassMeasurementRate rate);
 
     ///----------------------------------------------------------------------------------
     /// Reads the heading, pitch and roll from the compass.
